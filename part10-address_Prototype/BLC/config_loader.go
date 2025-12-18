ca8011d5a4769582f3972120eab9db8c58f2e74a
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-18 00:46:28 +0000

    Add configuration file loader with YAML parsing support

---
+++ part10-address_Prototype/BLC/config_loader.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+    "io/ioutil"
+    "log"
+
+    "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+        Name     string `yaml:"name"`
+    } `yaml:"database"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+    data, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+
+    var config Config
+    err = yaml.Unmarshal(data, &config)
+    if err != nil {
+        log.Printf("Failed to parse YAML: %v", err)
+        return nil, err
+    }
+
+    return &config, nil
+}

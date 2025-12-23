89f62c74d827b77076516c94b83afcecd2dbe187
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-23 10:03:56 +0000

    Add user activity logging middleware for tracking API usage

--- part5-cli-Prototype/BLC/user_activity_logger.go
+++ part5-cli-Prototype/BLC/user_activity_logger.go
@@ -148,3 +148,33 @@ func ExampleHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Request processed successfully"))
 }
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	
+	al.handler.ServeHTTP(w, r)
+	
+	duration := time.Since(start)
+	
+	log.Printf("Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
+}

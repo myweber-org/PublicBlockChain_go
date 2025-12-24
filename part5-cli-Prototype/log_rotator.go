241d08add828a5c46e18fd2d0a318038c50c9414
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-24 20:04:42 +0000

    Implement log rotation with size-based file splitting and compression

--- part5-cli-Prototype/log_rotator.go
+++ part5-cli-Prototype/log_rotator.go
@@ -277,4 +277,166 @@ func main() {
         logger.Write([]byte(msg))
         time.Sleep(10 * time.Millisecond)
     }
+}package main
+
+import (
+    "compress/gzip"
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "strconv"
+    "strings"
+    "sync"
+    "time"
+)
+
+type RotatingLogger struct {
+    mu          sync.Mutex
+    basePath    string
+    maxSize     int64
+    currentFile *os.File
+    currentSize int64
+    fileCount   int
+}
+
+func NewRotatingLogger(basePath string, maxSizeMB int) (*RotatingLogger, error) {
+    maxSize := int64(maxSizeMB) * 1024 * 1024
+    logger := &RotatingLogger{
+        basePath: basePath,
+        maxSize:  maxSize,
+    }
+    if err := logger.openCurrentFile(); err != nil {
+        return nil, err
+    }
+    return logger, nil
+}
+
+func (l *RotatingLogger) openCurrentFile() error {
+    file, err := os.OpenFile(l.basePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    info, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return err
+    }
+    l.currentFile = file
+    l.currentSize = info.Size()
+    return nil
+}
+
+func (l *RotatingLogger) Write(p []byte) (int, error) {
+    l.mu.Lock()
+    defer l.mu.Unlock()
+
+    if l.currentSize+int64(len(p)) > l.maxSize {
+        if err := l.rotate(); err != nil {
+            return 0, err
+        }
+    }
+
+    n, err := l.currentFile.Write(p)
+    if err == nil {
+        l.currentSize += int64(n)
+    }
+    return n, err
+}
+
+func (l *RotatingLogger) rotate() error {
+    if l.currentFile != nil {
+        l.currentFile.Close()
+    }
+
+    timestamp := time.Now().Format("20060102_150405")
+    archivePath := fmt.Sprintf("%s.%s.gz", l.basePath, timestamp)
+
+    if err := compressFile(l.basePath, archivePath); err != nil {
+        return err
+    }
+
+    if err := os.Remove(l.basePath); err != nil && !os.IsNotExist(err) {
+        return err
+    }
+
+    l.fileCount++
+    return l.openCurrentFile()
+}
+
+func compressFile(source, target string) error {
+    sourceFile, err := os.Open(source)
+    if err != nil {
+        return err
+    }
+    defer sourceFile.Close()
+
+    targetFile, err := os.Create(target)
+    if err != nil {
+        return err
+    }
+    defer targetFile.Close()
+
+    gzWriter := gzip.NewWriter(targetFile)
+    defer gzWriter.Close()
+
+    _, err = io.Copy(gzWriter, sourceFile)
+    return err
+}
+
+func (l *RotatingLogger) Close() error {
+    l.mu.Lock()
+    defer l.mu.Unlock()
+
+    if l.currentFile != nil {
+        return l.currentFile.Close()
+    }
+    return nil
+}
+
+func (l *RotatingLogger) CleanupOldFiles(maxFiles int) error {
+    l.mu.Lock()
+    defer l.mu.Unlock()
+
+    dir := filepath.Dir(l.basePath)
+    baseName := filepath.Base(l.basePath)
+
+    entries, err := os.ReadDir(dir)
+    if err != nil {
+        return err
+    }
+
+    var compressedFiles []string
+    for _, entry := range entries {
+        name := entry.Name()
+        if strings.HasPrefix(name, baseName+".") && strings.HasSuffix(name, ".gz") {
+            compressedFiles = append(compressedFiles, filepath.Join(dir, name))
+        }
+    }
+
+    if len(compressedFiles) <= maxFiles {
+        return nil
+    }
+
+    for i := 0; i < len(compressedFiles)-maxFiles; i++ {
+        os.Remove(compressedFiles[i])
+    }
+    return nil
+}
+
+func main() {
+    logger, err := NewRotatingLogger("app.log", 10)
+    if err != nil {
+        panic(err)
+    }
+    defer logger.Close()
+
+    for i := 0; i < 1000; i++ {
+        message := fmt.Sprintf("Log entry %d at %s\n", i, time.Now().Format(time.RFC3339))
+        logger.Write([]byte(message))
+        time.Sleep(10 * time.Millisecond)
+    }
+
+    logger.CleanupOldFiles(5)
+    fmt.Println("Log rotation completed. Check app.log.*.gz files.")
 }

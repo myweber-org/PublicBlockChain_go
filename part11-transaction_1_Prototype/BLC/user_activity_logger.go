d0b8ddea87be2b30d3d522ccba81b523a3023cbb
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-24 16:26:33 +0000

    Add user activity logging middleware for tracking API requests

--- part11-transaction_1_Prototype/BLC/user_activity_logger.go
+++ part11-transaction_1_Prototype/BLC/user_activity_logger.go
@@ -76,4 +76,87 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLog struct {
+	Timestamp time.Time
+	Method    string
+	Path      string
+	UserAgent string
+	IP        string
+	Duration  time.Duration
+}
+
+type ActivityLogger struct {
+	activities chan ActivityLog
+}
+
+func NewActivityLogger(bufferSize int) *ActivityLogger {
+	al := &ActivityLogger{
+		activities: make(chan ActivityLog, bufferSize),
+	}
+	go al.processLogs()
+	return al
+}
+
+func (al *ActivityLogger) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		
+		next.ServeHTTP(recorder, r)
+		
+		duration := time.Since(start)
+		
+		activity := ActivityLog{
+			Timestamp: time.Now(),
+			Method:    r.Method,
+			Path:      r.URL.Path,
+			UserAgent: r.UserAgent(),
+			IP:        r.RemoteAddr,
+			Duration:  duration,
+		}
+		
+		select {
+		case al.activities <- activity:
+		default:
+			log.Println("Activity log buffer full, dropping entry")
+		}
+	})
+}
+
+func (al *ActivityLogger) processLogs() {
+	for activity := range al.activities {
+		log.Printf("ACTIVITY: %s %s from %s (%s) took %v",
+			activity.Method,
+			activity.Path,
+			activity.IP,
+			activity.UserAgent,
+			activity.Duration,
+		)
+	}
+}
+
+func (al *ActivityLogger) Close() {
+	close(al.activities)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }

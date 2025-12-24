6e1e03c46c8cc8c05b5a261a69de16ffcff6d9f7
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-24 00:28:32 +0000

    Add user activity logging with timestamps and event types

--- part2-ProofOfWork-Prototype/user_activity_logger.go
+++ part2-ProofOfWork-Prototype/user_activity_logger.go
@@ -66,4 +66,71 @@ func main() {
     }
 
     fmt.Println("Activity logging completed")
+}package main
+
+import (
+    "encoding/json"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityEvent struct {
+    UserID    string    `json:"user_id"`
+    EventType string    `json:"event_type"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+    encoder *json.Encoder
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{
+        logFile: file,
+        encoder: json.NewEncoder(file),
+    }, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID, eventType, details string) error {
+    event := ActivityEvent{
+        UserID:    userID,
+        EventType: eventType,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+    return l.encoder.Encode(event)
+}
+
+func (l *ActivityLogger) Close() error {
+    return l.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("user_activity.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "Successful authentication")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
+
+    err = logger.LogActivity("user123", "view_page", "/dashboard")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
+
+    err = logger.LogActivity("user456", "purchase", "Product ID: 789")
+    if err != nil {
+        log.Println("Failed to log activity:", err)
+    }
 }

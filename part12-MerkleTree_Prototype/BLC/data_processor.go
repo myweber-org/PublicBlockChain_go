6fcc96d269c04c9efb8615de691bf03e12022b00
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2025-12-26 19:07:53 +0000

    Add data validation and sanitization functions for user input processing

--- part12-MerkleTree_Prototype/BLC/data_processor.go
+++ part12-MerkleTree_Prototype/BLC/data_processor.go
@@ -23,3 +23,28 @@ func TruncateString(input string, maxLength int) string {
 	}
 	return input[:maxLength]
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SanitizeUsername(input string) string {
+	re := regexp.MustCompile(`[^a-zA-Z0-9_-]`)
+	sanitized := re.ReplaceAllString(input, "")
+	return strings.TrimSpace(sanitized)
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
+}
+
+func TruncateString(s string, maxLen int) string {
+	if len(s) <= maxLen {
+		return s
+	}
+	return s[:maxLen]
+}

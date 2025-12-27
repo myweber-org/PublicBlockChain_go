2f16705286c4d9b2e952d65f8e54bfc9fbf0cfb2
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-27 20:16:51 +0000

    Add configuration loader with YAML parsing support

--- part13-Network_Prototype/config_loader.go
+++ part13-Network_Prototype/config_loader.go
@@ -2,58 +2,32 @@ package config
 
 import (
     "io/ioutil"
-    "log"
-
     "gopkg.in/yaml.v2"
 )
 
-type DatabaseConfig struct {
-    Host     string `yaml:"host"`
-    Port     int    `yaml:"port"`
-    Username string `yaml:"username"`
-    Password string `yaml:"password"`
-    Name     string `yaml:"name"`
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+        Name     string `yaml:"name"`
+    } `yaml:"database"`
 }
 
-type ServerConfig struct {
-    Port         int            `yaml:"port"`
-    Debug        bool           `yaml:"debug"`
-    ReadTimeout  int            `yaml:"read_timeout"`
-    WriteTimeout int            `yaml:"write_timeout"`
-}
-
-type AppConfig struct {
-    Database DatabaseConfig `yaml:"database"`
-    Server   ServerConfig   `yaml:"server"`
-}
-
-func LoadConfig(filePath string) (*AppConfig, error) {
-    data, err := ioutil.ReadFile(filePath)
+func LoadConfig(filename string) (*Config, error) {
+    data, err := ioutil.ReadFile(filename)
     if err != nil {
         return nil, err
     }
 
-    var config AppConfig
+    var config Config
     err = yaml.Unmarshal(data, &config)
     if err != nil {
         return nil, err
     }
 
     return &config, nil
-}
-
-func ValidateConfig(config *AppConfig) bool {
-    if config.Database.Host == "" {
-        log.Println("Database host is required")
-        return false
-    }
-    if config.Database.Port <= 0 {
-        log.Println("Database port must be positive")
-        return false
-    }
-    if config.Server.Port <= 0 {
-        log.Println("Server port must be positive")
-        return false
-    }
-    return true
 }

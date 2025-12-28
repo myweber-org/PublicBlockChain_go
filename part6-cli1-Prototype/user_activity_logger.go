dd7e8a3a81634867cac11ca174a879fc98051457
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-28 20:43:15 +0000

    Add user activity logging middleware for tracking API usage

--- part6-cli1-Prototype/user_activity_logger.go
+++ part6-cli1-Prototype/user_activity_logger.go
@@ -1,3 +1,4 @@
+
 package middleware
 
 import (
@@ -25,63 +26,13 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	duration := time.Since(start)
 	log.Printf(
-		"%s %s %d %s %s",
+		"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
 		r.Method,
 		r.URL.Path,
 		recorder.statusCode,
 		duration,
-		r.RemoteAddr,
+		r.UserAgent(),
 	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		userAgent := r.UserAgent()
-		clientIP := r.RemoteAddr
-
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-
-		next.ServeHTTP(recorder, r)
-
-		duration := time.Since(start)
-		al.Logger.Printf(
-			"Method: %s | Path: %s | Status: %d | Duration: %v | IP: %s | Agent: %s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			clientIP,
-			userAgent,
-		)
-	})
 }
 
 type responseRecorder struct {

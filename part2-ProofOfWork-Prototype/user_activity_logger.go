0cdeb9c881e7d566ff6a08a2789900be91a6945a
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2025-12-29 14:50:46 +0000

    Add user activity logging middleware to track API usage

--- part2-ProofOfWork-Prototype/user_activity_logger.go
+++ part2-ProofOfWork-Prototype/user_activity_logger.go
@@ -7,55 +7,6 @@ import (
 )
 
 type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"Method=%s Path=%s Status=%d Duration=%s RemoteAddr=%s UserAgent=%s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-			r.UserAgent(),
-		)
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
 	handler http.Handler
 }
 
@@ -65,32 +16,14 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-	
-	al.handler.ServeHTTP(recorder, r)
-	
+	al.handler.ServeHTTP(w, r)
 	duration := time.Since(start)
-	
+
 	log.Printf(
-		"%s %s %d %s %s",
+		"Method: %s, Path: %s, Duration: %v, Timestamp: %s",
 		r.Method,
 		r.URL.Path,
-		recorder.statusCode,
 		duration,
-		r.RemoteAddr,
+		time.Now().Format(time.RFC3339),
 	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
 }

c9aec91a8871d918954bc35ca79f0d1ef7d03ada
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-30 23:37:23 +0000

    Add log rotation with size-based file splitting and cleanup

--- part13-Network_Prototype/BLC/log_rotator.go
+++ part13-Network_Prototype/BLC/log_rotator.go
@@ -88,4 +88,132 @@ func main() {
 
 	rotator.CleanOldBackups()
 	fmt.Println("Log rotation completed")
+}package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "sort"
+    "strings"
+    "time"
+)
+
+const (
+    maxFileSize    = 10 * 1024 * 1024 // 10MB
+    maxBackupFiles = 5
+    logDir         = "./logs"
+)
+
+type LogRotator struct {
+    currentFile *os.File
+    filePath    string
+    bytesWritten int64
+}
+
+func NewLogRotator(baseName string) (*LogRotator, error) {
+    if err := os.MkdirAll(logDir, 0755); err != nil {
+        return nil, err
+    }
+
+    filePath := filepath.Join(logDir, baseName+".log")
+    file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+
+    info, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return nil, err
+    }
+
+    return &LogRotator{
+        currentFile:  file,
+        filePath:     filePath,
+        bytesWritten: info.Size(),
+    }, nil
+}
+
+func (lr *LogRotator) Write(p []byte) (int, error) {
+    if lr.bytesWritten+int64(len(p)) > maxFileSize {
+        if err := lr.rotate(); err != nil {
+            return 0, err
+        }
+    }
+
+    n, err := lr.currentFile.Write(p)
+    if err == nil {
+        lr.bytesWritten += int64(n)
+    }
+    return n, err
+}
+
+func (lr *LogRotator) rotate() error {
+    if err := lr.currentFile.Close(); err != nil {
+        return err
+    }
+
+    timestamp := time.Now().Format("20060102_150405")
+    backupPath := strings.TrimSuffix(lr.filePath, ".log") + "_" + timestamp + ".log"
+
+    if err := os.Rename(lr.filePath, backupPath); err != nil {
+        return err
+    }
+
+    file, err := os.OpenFile(lr.filePath, os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+
+    lr.currentFile = file
+    lr.bytesWritten = 0
+
+    go lr.cleanupOldFiles()
+    return nil
+}
+
+func (lr *LogRotator) cleanupOldFiles() {
+    pattern := filepath.Join(logDir, strings.TrimSuffix(filepath.Base(lr.filePath), ".log") + "_*.log")
+    matches, err := filepath.Glob(pattern)
+    if err != nil {
+        return
+    }
+
+    if len(matches) <= maxBackupFiles {
+        return
+    }
+
+    sort.Strings(matches)
+    filesToDelete := matches[:len(matches)-maxBackupFiles]
+
+    for _, file := range filesToDelete {
+        os.Remove(file)
+    }
+}
+
+func (lr *LogRotator) Close() error {
+    return lr.currentFile.Close()
+}
+
+func main() {
+    rotator, err := NewLogRotator("application")
+    if err != nil {
+        fmt.Printf("Failed to create log rotator: %v\n", err)
+        return
+    }
+    defer rotator.Close()
+
+    for i := 0; i < 100; i++ {
+        logEntry := fmt.Sprintf("[%s] Log entry %d: Test message for rotation\n", 
+            time.Now().Format(time.RFC3339), i)
+        if _, err := rotator.Write([]byte(logEntry)); err != nil {
+            fmt.Printf("Write error: %v\n", err)
+            break
+        }
+        time.Sleep(10 * time.Millisecond)
+    }
+
+    fmt.Println("Log rotation test completed")
 }

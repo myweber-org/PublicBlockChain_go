28d2434e614b24c4dee1df54c567ae8939c14ec4
YourApexArchive <your.apex.archive@hotmail.com>
2025-12-30 21:13:41 +0000

    Add user activity logging middleware for API endpoints

--- part2-ProofOfWork-Prototype/BLC/user_activity_logger.go
+++ part2-ProofOfWork-Prototype/BLC/user_activity_logger.go
@@ -1,43 +1,3 @@
-
-package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLog struct {
-	Timestamp time.Time
-	Method    string
-	Path      string
-	UserAgent string
-	IP        string
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		activity := ActivityLog{
-			Timestamp: start,
-			Method:    r.Method,
-			Path:      r.URL.Path,
-			UserAgent: r.UserAgent(),
-			IP:        r.RemoteAddr,
-		}
-
-		log.Printf("Activity: %s %s from %s (%s) at %s",
-			activity.Method,
-			activity.Path,
-			activity.IP,
-			activity.UserAgent,
-			activity.Timestamp.Format(time.RFC3339),
-		)
-
-		next.ServeHTTP(w, r)
-	})
-}
 package middleware
 
 import (
@@ -56,12 +16,13 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	userAgent := r.UserAgent()
-	clientIP := r.RemoteAddr
-
 	al.handler.ServeHTTP(w, r)
-
 	duration := time.Since(start)
-	log.Printf("Activity: %s %s | User-Agent: %s | IP: %s | Duration: %v",
-		r.Method, r.URL.Path, userAgent, clientIP, duration)
+
+	log.Printf("Activity: %s %s from %s took %v",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+	)
 }

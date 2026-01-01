cea160b21ec436d2f7bb24464f7caee4b1bb7200
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-01 20:03:08 +0000

    Add user activity logging middleware for tracking API usage

--- part10-address_Prototype/user_activity_logger.go
+++ part10-address_Prototype/user_activity_logger.go
@@ -7,86 +7,44 @@ import (
 )
 
 type ActivityLogger struct {
-	handler http.Handler
+	Logger *log.Logger
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
-
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
-}
-package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		
+		next.ServeHTTP(recorder, r)
+		
+		duration := time.Since(start)
+		
+		al.Logger.Printf(
+			"[%s] %s %s %d %s %s",
+			time.Now().Format(time.RFC3339),
+			r.Method,
+			r.URL.Path,
+			recorder.statusCode,
+			duration,
+			r.RemoteAddr,
+		)
+	})
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	userID := extractUserID(r)
-	ipAddress := r.RemoteAddr
-	method := r.Method
-	path := r.URL.Path
-
-	al.handler.ServeHTTP(w, r)
-
-	duration := time.Since(start)
-	log.Printf("User %s from %s performed %s %s in %v", userID, ipAddress, method, path, duration)
-}
-
-func extractUserID(r *http.Request) string {
-	if userID := r.Header.Get("X-User-ID"); userID != "" {
-		return userID
-	}
-	return "anonymous"
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }

8af68e663df75b06a9f6ff9df5e1bc5877ac6a33
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-04 15:12:38 +0000

    Add configuration loader with environment variable support and validation

--- part13-Network_Prototype/config_loader.go
+++ part13-Network_Prototype/config_loader.go
@@ -1,33 +1,85 @@
 package config
 
 import (
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type Config struct {
-    Server struct {
-        Host string `yaml:"host"`
-        Port int    `yaml:"port"`
-    } `yaml:"server"`
-    Database struct {
-        Username string `yaml:"username"`
-        Password string `yaml:"password"`
-        Name     string `yaml:"name"`
-    } `yaml:"database"`
+	ServerPort int
+	DBHost     string
+	DBPort     int
+	DebugMode  bool
+	APIKeys    []string
 }
 
-func LoadConfig(filename string) (*Config, error) {
-    data, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
+func LoadConfig() (*Config, error) {
+	cfg := &Config{}
+	var err error
 
-    var config Config
-    err = yaml.Unmarshal(data, &config)
-    if err != nil {
-        return nil, err
-    }
+	cfg.ServerPort, err = getEnvInt("SERVER_PORT", 8080)
+	if err != nil {
+		return nil, err
+	}
 
-    return &config, nil
+	cfg.DBHost = getEnvString("DB_HOST", "localhost")
+	
+	cfg.DBPort, err = getEnvInt("DB_PORT", 5432)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.DebugMode, err = getEnvBool("DEBUG_MODE", false)
+	if err != nil {
+		return nil, err
+	}
+
+	apiKeysStr := getEnvString("API_KEYS", "")
+	if apiKeysStr != "" {
+		cfg.APIKeys = strings.Split(apiKeysStr, ",")
+		for i, key := range cfg.APIKeys {
+			cfg.APIKeys[i] = strings.TrimSpace(key)
+		}
+	}
+
+	if cfg.ServerPort < 1 || cfg.ServerPort > 65535 {
+		return nil, errors.New("invalid server port range")
+	}
+
+	if cfg.DBPort < 1 || cfg.DBPort > 65535 {
+		return nil, errors.New("invalid database port range")
+	}
+
+	return cfg, nil
+}
+
+func getEnvString(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvInt(key string, defaultValue int) (int, error) {
+	if value, exists := os.LookupEnv(key); exists {
+		intValue, err := strconv.Atoi(value)
+		if err != nil {
+			return 0, errors.New("invalid integer value for " + key)
+		}
+		return intValue, nil
+	}
+	return defaultValue, nil
+}
+
+func getEnvBool(key string, defaultValue bool) (bool, error) {
+	if value, exists := os.LookupEnv(key); exists {
+		boolValue, err := strconv.ParseBool(value)
+		if err != nil {
+			return false, errors.New("invalid boolean value for " + key)
+		}
+		return boolValue, nil
+	}
+	return defaultValue, nil
 }

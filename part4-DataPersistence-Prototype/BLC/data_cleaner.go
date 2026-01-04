58ce3e85b6b00d8119c8cea90ecd1c849610bc56
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-04 22:30:22 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- part4-DataPersistence-Prototype/BLC/data_cleaner.go
+++ part4-DataPersistence-Prototype/BLC/data_cleaner.go
@@ -3,43 +3,22 @@ package main
 
 import "fmt"
 
-func RemoveDuplicates(input []int) []int {
-	seen := make(map[int]bool)
-	result := []int{}
-
-	for _, value := range input {
-		if !seen[value] {
-			seen[value] = true
-			result = append(result, value)
-		}
-	}
-	return result
+func RemoveDuplicates(nums []int) []int {
+    seen := make(map[int]bool)
+    result := []int{}
+    
+    for _, num := range nums {
+        if !seen[num] {
+            seen[num] = true
+            result = append(result, num)
+        }
+    }
+    return result
 }
 
 func main() {
-	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
-	cleaned := RemoveDuplicates(data)
-	fmt.Printf("Original: %v\n", data)
-	fmt.Printf("Cleaned: %v\n", cleaned)
-}package main
-
-import "fmt"
-
-func RemoveDuplicates(input []string) []string {
-	seen := make(map[string]bool)
-	result := []string{}
-	for _, item := range input {
-		if !seen[item] {
-			seen[item] = true
-			result = append(result, item)
-		}
-	}
-	return result
-}
-
-func main() {
-	data := []string{"apple", "banana", "apple", "orange", "banana", "grape"}
-	cleaned := RemoveDuplicates(data)
-	fmt.Println("Original:", data)
-	fmt.Println("Cleaned:", cleaned)
+    data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+    cleaned := RemoveDuplicates(data)
+    fmt.Println("Original:", data)
+    fmt.Println("Cleaned:", cleaned)
 }

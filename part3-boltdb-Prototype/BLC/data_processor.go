d5833f8c8cec1a0b18b143bca3dc758899770212
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-05 21:10:48 +0000

    Add data processing utility for cleaning and validating input strings

--- part3-boltdb-Prototype/BLC/data_processor.go
+++ part3-boltdb-Prototype/BLC/data_processor.go
@@ -95,3 +95,26 @@ func main() {
 		os.Exit(1)
 	}
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func CleanInput(input string) string {
+	trimmed := strings.TrimSpace(input)
+	re := regexp.MustCompile(`\s+`)
+	return re.ReplaceAllString(trimmed, " ")
+}
+
+func ValidateEmail(email string) bool {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
+}
+
+func ContainsOnlyAlphanumeric(s string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+	return re.MatchString(s)
+}

94087f4e884f78a9bd61a9b1b36f9c40a5001d88
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-06 19:31:53 +0000

    Add file encryption utility with AES-256-CBC support

--- part11-transaction_1_Prototype/BLC/file_encryptor.go
+++ part11-transaction_1_Prototype/BLC/file_encryptor.go
@@ -246,4 +246,119 @@ func main() {
 		fmt.Println("Invalid operation. Use 'encrypt' or 'decrypt'")
 		os.Exit(1)
 	}
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+)
+
+func encryptFile(inputPath, outputPath, keyString string) error {
+	key, _ := hex.DecodeString(keyString)
+	if len(key) != 32 {
+		return fmt.Errorf("key must be 32 bytes for AES-256")
+	}
+
+	plaintext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return os.WriteFile(outputPath, ciphertext, 0644)
+}
+
+func decryptFile(inputPath, outputPath, keyString string) error {
+	key, _ := hex.DecodeString(keyString)
+	if len(key) != 32 {
+		return fmt.Errorf("key must be 32 bytes for AES-256")
+	}
+
+	ciphertext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return fmt.Errorf("ciphertext too short")
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return os.WriteFile(outputPath, ciphertext, 0644)
+}
+
+func generateRandomKey() string {
+	key := make([]byte, 32)
+	if _, err := rand.Read(key); err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(key)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_encryptor <command>")
+		fmt.Println("Commands:")
+		fmt.Println("  genkey - generate random encryption key")
+		fmt.Println("  encrypt <input> <output> <key>")
+		fmt.Println("  decrypt <input> <output> <key>")
+		return
+	}
+
+	switch os.Args[1] {
+	case "genkey":
+		fmt.Println("Generated key:", generateRandomKey())
+	case "encrypt":
+		if len(os.Args) != 5 {
+			fmt.Println("Usage: file_encryptor encrypt <input> <output> <key>")
+			return
+		}
+		err := encryptFile(os.Args[2], os.Args[3], os.Args[4])
+		if err != nil {
+			fmt.Println("Encryption failed:", err)
+		} else {
+			fmt.Println("File encrypted successfully")
+		}
+	case "decrypt":
+		if len(os.Args) != 5 {
+			fmt.Println("Usage: file_encryptor decrypt <input> <output> <key>")
+			return
+		}
+		err := decryptFile(os.Args[2], os.Args[3], os.Args[4])
+		if err != nil {
+			fmt.Println("Decryption failed:", err)
+		} else {
+			fmt.Println("File decrypted successfully")
+		}
+	default:
+		fmt.Println("Unknown command:", os.Args[1])
+	}
 }

a382b2c18c288a6b4565eb62253c03419f43157b
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-07 21:02:33 +0000

    Add log rotation with size-based file splitting and cleanup

--- part3-boltdb-Prototype/log_rotator.go
+++ part3-boltdb-Prototype/log_rotator.go
@@ -433,4 +433,123 @@ func main() {
 		logger.Write([]byte(message))
 		time.Sleep(10 * time.Millisecond)
 	}
+}package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	maxFileSize = 10 * 1024 * 1024
+	maxBackups  = 5
+	logFileName = "app.log"
+)
+
+type LogRotator struct {
+	currentFile *os.File
+	currentSize int64
+	basePath    string
+}
+
+func NewLogRotator(baseDir string) (*LogRotator, error) {
+	if err := os.MkdirAll(baseDir, 0755); err != nil {
+		return nil, err
+	}
+
+	fullPath := filepath.Join(baseDir, logFileName)
+	file, err := os.OpenFile(fullPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
+
+	return &LogRotator{
+		currentFile: file,
+		currentSize: info.Size(),
+		basePath:    baseDir,
+	}, nil
+}
+
+func (lr *LogRotator) Write(p []byte) (int, error) {
+	if lr.currentSize+int64(len(p)) > maxFileSize {
+		if err := lr.rotate(); err != nil {
+			return 0, err
+		}
+	}
+
+	n, err := lr.currentFile.Write(p)
+	if err == nil {
+		lr.currentSize += int64(n)
+	}
+	return n, err
+}
+
+func (lr *LogRotator) rotate() error {
+	if err := lr.currentFile.Close(); err != nil {
+		return err
+	}
+
+	timestamp := time.Now().Format("20060102_150405")
+	oldPath := filepath.Join(lr.basePath, logFileName)
+	newPath := filepath.Join(lr.basePath, fmt.Sprintf("app_%s.log", timestamp))
+
+	if err := os.Rename(oldPath, newPath); err != nil {
+		return err
+	}
+
+	file, err := os.OpenFile(oldPath, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	lr.currentFile = file
+	lr.currentSize = 0
+
+	go lr.cleanup()
+	return nil
+}
+
+func (lr *LogRotator) cleanup() {
+	pattern := filepath.Join(lr.basePath, "app_*.log")
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return
+	}
+
+	if len(matches) <= maxBackups {
+		return
+	}
+
+	for i := 0; i < len(matches)-maxBackups; i++ {
+		os.Remove(matches[i])
+	}
+}
+
+func (lr *LogRotator) Close() error {
+	return lr.currentFile.Close()
+}
+
+func main() {
+	rotator, err := NewLogRotator("./logs")
+	if err != nil {
+		panic(err)
+	}
+	defer rotator.Close()
+
+	for i := 0; i < 1000; i++ {
+		msg := fmt.Sprintf("Log entry %d: %s\n", i, time.Now().Format(time.RFC3339))
+		rotator.Write([]byte(msg))
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	fmt.Println("Log rotation completed")
 }

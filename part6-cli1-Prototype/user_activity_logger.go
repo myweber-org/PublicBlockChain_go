76a7238b91bbb2ca4aea8a72a9c81418ae20c5ce
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-08 01:11:35 +0000

    Add user activity logging with timestamps and event types

--- part6-cli1-Prototype/user_activity_logger.go
+++ part6-cli1-Prototype/user_activity_logger.go
@@ -43,4 +43,72 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityEvent struct {
+    UserID    string    `json:"user_id"`
+    EventType string    `json:"event_type"`
+    Timestamp time.Time `json:"timestamp"`
+    Metadata  string    `json:"metadata,omitempty"`
+}
+
+type ActivityLogger struct {
+    logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return nil, err
+    }
+    return &ActivityLogger{logFile: file}, nil
+}
+
+func (al *ActivityLogger) LogActivity(userID, eventType, metadata string) error {
+    event := ActivityEvent{
+        UserID:    userID,
+        EventType: eventType,
+        Timestamp: time.Now(),
+        Metadata:  metadata,
+    }
+
+    data, err := json.Marshal(event)
+    if err != nil {
+        return err
+    }
+
+    _, err = al.logFile.Write(append(data, '\n'))
+    return err
+}
+
+func (al *ActivityLogger) Close() error {
+    return al.logFile.Close()
+}
+
+func main() {
+    logger, err := NewActivityLogger("activity.log")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer logger.Close()
+
+    err = logger.LogActivity("user123", "login", "browser:chrome")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = logger.LogActivity("user123", "view_page", "page:/dashboard")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("Activity logged successfully")
 }

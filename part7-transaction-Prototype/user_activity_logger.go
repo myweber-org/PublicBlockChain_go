e73327fc61113b821bab1d25e3e5d6af6700b75f
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-09 23:25:38 +0000

    Add user activity logging middleware for tracking API usage

--- part7-transaction-Prototype/user_activity_logger.go
+++ part7-transaction-Prototype/user_activity_logger.go
@@ -28,11 +28,12 @@ func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
 		duration := time.Since(start)
 		
 		al.Logger.Printf(
-			"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
+			"Method: %s | Path: %s | Status: %d | Duration: %v | RemoteAddr: %s | UserAgent: %s",
 			r.Method,
 			r.URL.Path,
 			recorder.statusCode,
 			duration,
+			r.RemoteAddr,
 			r.UserAgent(),
 		)
 	})
@@ -46,95 +47,4 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.UserAgent(),
-		)
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package main
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
-
-	log.Printf("[%s] %s %s - %v - %s",
-		time.Now().Format("2006-01-02 15:04:05"),
-		r.Method,
-		r.URL.Path,
-		duration,
-		r.RemoteAddr)
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func apiHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"status": "ok"}`))
-}
-
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/api/data", apiHandler)
-
-	wrappedMux := NewActivityLogger(mux)
-
-	log.Println("Server starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", wrappedMux))
 }

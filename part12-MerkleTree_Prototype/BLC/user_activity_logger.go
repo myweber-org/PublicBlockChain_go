c85093c0e23fb9053627046d58b348bad95913e6
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-11 23:09:39 +0000

    Add user activity logging middleware for API endpoints

--- part12-MerkleTree_Prototype/BLC/user_activity_logger.go
+++ part12-MerkleTree_Prototype/BLC/user_activity_logger.go
@@ -6,41 +6,6 @@ import (
 	"time"
 )
 
-type responseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-
-		next.ServeHTTP(rw, r)
-
-		duration := time.Since(start)
-		log.Printf(
-			"%s - %s %s %d %v",
-			r.RemoteAddr,
-			r.Method,
-			r.URL.Path,
-			rw.statusCode,
-			duration,
-		)
-	})
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
 type ActivityLogger struct {
 	handler http.Handler
 }
@@ -54,11 +19,10 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	al.handler.ServeHTTP(w, r)
 	duration := time.Since(start)
 
-	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
+	log.Printf("Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
+		r.RemoteAddr,
 		duration,
-		time.Now().Format(time.RFC3339),
 	)
 }

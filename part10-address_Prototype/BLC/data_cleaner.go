629d7670290f38409f6ccb5ca43d4359ef25ce88
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-11 22:29:22 +0000

    Add data cleaning utility to remove duplicate entries from a slice

--- part10-address_Prototype/BLC/data_cleaner.go
+++ part10-address_Prototype/BLC/data_cleaner.go
@@ -289,4 +289,17 @@ func RemoveDuplicates[T comparable](slice []T) []T {
 		}
 	}
 	return result
+}package utils
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+	return result
 }

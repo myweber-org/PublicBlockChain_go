583ca54d1ed74dd6bd7beeacad85b9d48cb7c12f
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-12 01:13:24 +0000

    Add user activity logging with timestamp and action type

--- part1-Basic-Prototype/BLC/user_activity_logger.go
+++ part1-Basic-Prototype/BLC/user_activity_logger.go
@@ -1,29 +1,72 @@
-package middleware
+package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-	"net/http"
+	"os"
 	"time"
 )
 
+type ActivityLog struct {
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Details   string    `json:"details,omitempty"`
+}
+
 type ActivityLogger struct {
-	handler http.Handler
+	logFile *os.File
+	encoder *json.Encoder
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{
+		logFile: file,
+		encoder: json.NewEncoder(file),
+	}, nil
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
+func (l *ActivityLogger) LogActivity(userID, action, details string) error {
+	entry := ActivityLog{
+		Timestamp: time.Now().UTC(),
+		UserID:    userID,
+		Action:    action,
+		Details:   details,
+	}
+	return l.encoder.Encode(entry)
+}
 
-	log.Printf("[%s] %s %s %s %v",
-		time.Now().Format("2006-01-02 15:04:05"),
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
+func (l *ActivityLogger) Close() error {
+	return l.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	activities := []struct {
+		userID  string
+		action  string
+		details string
+	}{
+		{"user_001", "LOGIN", "Successful authentication"},
+		{"user_002", "UPLOAD", "File: report.pdf"},
+		{"user_001", "LOGOUT", "Session ended"},
+	}
+
+	for _, act := range activities {
+		if err := logger.LogActivity(act.userID, act.action, act.details); err != nil {
+			fmt.Printf("Failed to log activity: %v\n", err)
+		}
+	}
+
+	fmt.Println("Activity logging completed")
 }

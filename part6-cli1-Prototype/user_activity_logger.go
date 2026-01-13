211c7d59c2ed979b8df0113bdf0893db296309aa
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-13 16:03:08 +0000

    Add user activity logging middleware with request details and timestamp

--- part6-cli1-Prototype/user_activity_logger.go
+++ part6-cli1-Prototype/user_activity_logger.go
@@ -1,4 +1,3 @@
-
 package middleware
 
 import (
@@ -7,152 +6,32 @@ import (
 	"time"
 )
 
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-
-	al.handler.ServeHTTP(recorder, r)
-
-	duration := time.Since(start)
-	log.Printf(
-		"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
-		r.Method,
-		r.URL.Path,
-		recorder.statusCode,
-		duration,
-		r.UserAgent(),
-	)
-}
-
-type responseRecorder struct {
+type responseWriter struct {
 	http.ResponseWriter
 	statusCode int
 }
 
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package main
-
-import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "os"
-    "time"
-)
-
-type ActivityEvent struct {
-    UserID    string    `json:"user_id"`
-    EventType string    `json:"event_type"`
-    Timestamp time.Time `json:"timestamp"`
-    Metadata  string    `json:"metadata,omitempty"`
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
 
-type ActivityLogger struct {
-    logFile *os.File
-}
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
 
-func NewActivityLogger(filename string) (*ActivityLogger, error) {
-    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return nil, err
-    }
-    return &ActivityLogger{logFile: file}, nil
-}
+		next.ServeHTTP(rw, r)
 
-func (al *ActivityLogger) LogActivity(userID, eventType, metadata string) error {
-    event := ActivityEvent{
-        UserID:    userID,
-        EventType: eventType,
-        Timestamp: time.Now(),
-        Metadata:  metadata,
-    }
-
-    data, err := json.Marshal(event)
-    if err != nil {
-        return err
-    }
-
-    _, err = al.logFile.Write(append(data, '\n'))
-    return err
-}
-
-func (al *ActivityLogger) Close() error {
-    return al.logFile.Close()
-}
-
-func main() {
-    logger, err := NewActivityLogger("activity.log")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer logger.Close()
-
-    err = logger.LogActivity("user123", "login", "browser:chrome")
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    err = logger.LogActivity("user123", "view_page", "page:/dashboard")
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    fmt.Println("Activity logged successfully")
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-
-	al.handler.ServeHTTP(recorder, r)
-
-	duration := time.Since(start)
-	log.Printf(
-		"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
-		r.Method,
-		r.URL.Path,
-		recorder.statusCode,
-		duration,
-		r.UserAgent(),
-	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
+		duration := time.Since(start)
+		log.Printf(
+			"Method: %s | Path: %s | Status: %d | Duration: %v | RemoteAddr: %s | UserAgent: %s",
+			r.Method,
+			r.URL.Path,
+			rw.statusCode,
+			duration,
+			r.RemoteAddr,
+			r.Header.Get("User-Agent"),
+		)
+	})
 }

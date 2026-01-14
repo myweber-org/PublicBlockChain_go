c28b406d551b238c951e48dcd6681f56b7748637
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-14 00:32:15 +0000

    Add data validation and sanitization logic for user input

--- part2-ProofOfWork-Prototype/BLC/data_processor.go
+++ part2-ProofOfWork-Prototype/BLC/data_processor.go
@@ -163,4 +163,37 @@ func main() {
 	}
 
 	fmt.Printf("Processed user: ID=%d, Name=%s, Email=%s\n", user.ID, user.Name, user.Email)
+}package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	emailRegex *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	regex := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+	return &DataProcessor{emailRegex: regex}
+}
+
+func (dp *DataProcessor) SanitizeInput(input string) string {
+	input = strings.TrimSpace(input)
+	input = strings.ReplaceAll(input, "<", "&lt;")
+	input = strings.ReplaceAll(input, ">", "&gt;")
+	return input
+}
+
+func (dp *DataProcessor) ValidateEmail(email string) bool {
+	return dp.emailRegex.MatchString(email)
+}
+
+func (dp *DataProcessor) ProcessUserData(name, email string) (string, bool) {
+	sanitizedName := dp.SanitizeInput(name)
+	if !dp.ValidateEmail(email) {
+		return "", false
+	}
+	return sanitizedName, true
 }

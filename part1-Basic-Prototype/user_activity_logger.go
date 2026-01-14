e18d5d0be76d97a18a7fd02aa3e0016138e55130
InkspillVerse <inkspill.verse@outlook.com>
2026-01-14 20:17:20 +0000

    Add user activity logging middleware for API endpoints

--- part1-Basic-Prototype/user_activity_logger.go
+++ part1-Basic-Prototype/user_activity_logger.go
@@ -6,56 +6,24 @@ import (
 	"time"
 )
 
-type ActivityLog struct {
-	UserID    string
-	IPAddress string
-	Method    string
-	Path      string
-	Timestamp time.Time
+type ActivityLogger struct {
+	handler http.Handler
 }
 
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-
-		userID := "anonymous"
-		if auth := r.Header.Get("Authorization"); auth != "" {
-			userID = extractUserID(auth)
-		}
-
-		activity := ActivityLog{
-			UserID:    userID,
-			IPAddress: r.RemoteAddr,
-			Method:    r.Method,
-			Path:      r.URL.Path,
-			Timestamp: start,
-		}
-
-		logActivity(activity)
-
-		next.ServeHTTP(w, r)
-	})
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func extractUserID(authHeader string) string {
-	if len(authHeader) > 7 && authHeader[:7] == "Bearer " {
-		return authHeader[7:min(len(authHeader), 20)] + "..."
-	}
-	return "authenticated"
-}
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
 
-func logActivity(activity ActivityLog) {
-	log.Printf("ACTIVITY: User=%s IP=%s %s %s at %s",
-		activity.UserID,
-		activity.IPAddress,
-		activity.Method,
-		activity.Path,
-		activity.Timestamp.Format(time.RFC3339))
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+	log.Printf(
+		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
+		r.Method,
+		r.URL.Path,
+		duration,
+		time.Now().Format(time.RFC3339),
+	)
 }

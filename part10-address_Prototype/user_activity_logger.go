64798636bd36df99145e963f02d5c84b9dde89ba
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-15 00:32:28 +0000

    Add user activity logging middleware for tracking API usage

--- part10-address_Prototype/user_activity_logger.go
+++ part10-address_Prototype/user_activity_logger.go
@@ -7,55 +7,6 @@ import (
 )
 
 type ActivityLogger struct {
-	Logger *log.Logger
-}
-
-func NewActivityLogger(logger *log.Logger) *ActivityLogger {
-	return &ActivityLogger{Logger: logger}
-}
-
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		recorder := &responseRecorder{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-		
-		next.ServeHTTP(recorder, r)
-		
-		duration := time.Since(start)
-		
-		al.Logger.Printf(
-			"Method=%s Path=%s Status=%d Duration=%s RemoteAddr=%s UserAgent=%s",
-			r.Method,
-			r.URL.Path,
-			recorder.statusCode,
-			duration,
-			r.RemoteAddr,
-			r.UserAgent(),
-		)
-	})
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
 	handler http.Handler
 }
 
@@ -69,10 +20,10 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	duration := time.Since(start)
 
 	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | User-Agent: %s",
+		"Activity: %s %s from %s took %v",
 		r.Method,
 		r.URL.Path,
+		r.RemoteAddr,
 		duration,
-		r.UserAgent(),
 	)
 }

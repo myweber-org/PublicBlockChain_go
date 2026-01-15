4c4a1585d3964044f35d7a6a15da7c7b1e540365
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-15 15:06:17 +0000

    Add utility to clean up temporary files in a directory

--- part10-address_Prototype/BLC/file_cleaner.go
+++ part10-address_Prototype/BLC/file_cleaner.go
@@ -7,25 +7,47 @@ import (
 	"time"
 )
 
-func main() {
-	tempDir := os.TempDir()
-	cutoff := time.Now().AddDate(0, 0, -7)
+const tempFileAgeThreshold = 24 * time.Hour
 
-	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_cleaner <directory>")
+		os.Exit(1)
+	}
+
+	dir := os.Args[1]
+	err := cleanTempFiles(dir)
+	if err != nil {
+		fmt.Printf("Error cleaning directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanTempFiles(dir string) error {
+	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			return nil
+			return err
 		}
+
 		if info.IsDir() {
 			return nil
 		}
-		if info.ModTime().Before(cutoff) {
-			fmt.Printf("Removing old file: %s\n", path)
-			os.Remove(path)
+
+		if isTempFile(info.Name()) && isOldFile(info.ModTime()) {
+			fmt.Printf("Removing: %s\n", path)
+			return os.Remove(path)
 		}
+
 		return nil
 	})
+}
 
-	if err != nil {
-		fmt.Printf("Error cleaning temp directory: %v\n", err)
-	}
+func isTempFile(filename string) bool {
+	return len(filename) > 4 && filename[len(filename)-4:] == ".tmp"
+}
+
+func isOldFile(modTime time.Time) bool {
+	return time.Since(modTime) > tempFileAgeThreshold
 }

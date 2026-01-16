06c2bf47c7d7181788d93f72afec8aa240460110
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-16 23:36:43 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- part7-transaction-Prototype/BLC/data_cleaner.go
+++ part7-transaction-Prototype/BLC/data_cleaner.go
@@ -40,3 +40,25 @@ func main() {
 	fmt.Println("Original:", strings)
 	fmt.Println("Unique:", uniqueStrings)
 }
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	input := []int{1, 2, 2, 3, 4, 4, 5, 6, 6, 7}
+	cleaned := removeDuplicates(input)
+	fmt.Println("Original:", input)
+	fmt.Println("Cleaned:", cleaned)
+}

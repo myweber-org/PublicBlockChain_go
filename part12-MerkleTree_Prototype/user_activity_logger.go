4f8edbb7a3ccc4aac42907b8dc12bdfaf9375e97
InkspillVerse <inkspill.verse@outlook.com>
2026-01-17 16:53:14 +0000

    Add user activity logging middleware for tracking API usage

--- part12-MerkleTree_Prototype/user_activity_logger.go
+++ part12-MerkleTree_Prototype/user_activity_logger.go
@@ -256,4 +256,34 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		duration,
 		time.Now().Format(time.RFC3339),
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	Logger *log.Logger
+}
+
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
+}
+
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		userAgent := r.UserAgent()
+		clientIP := r.RemoteAddr
+		method := r.Method
+		path := r.URL.Path
+
+		next.ServeHTTP(w, r)
+
+		duration := time.Since(start)
+		al.Logger.Printf("Activity: %s %s | Client: %s | Agent: %s | Duration: %v",
+			method, path, clientIP, userAgent, duration)
+	})
 }

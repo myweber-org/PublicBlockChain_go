fa3bc26631c5998409c56662be659e084d1e1b73
YourApexArchive <your.apex.archive@hotmail.com>
2026-01-17 23:15:06 +0000

    Add data cleaning utility to remove duplicate entries from slices

--- part12-MerkleTree_Prototype/data_cleaner.go
+++ part12-MerkleTree_Prototype/data_cleaner.go
@@ -111,4 +111,18 @@ func main() {
 	uniqueStrings := RemoveDuplicates(strings)
 	fmt.Println("Original:", strings)
 	fmt.Println("Unique:", uniqueStrings)
+}package data
+
+func RemoveDuplicates[T comparable](slice []T) []T {
+	seen := make(map[T]bool)
+	result := []T{}
+
+	for _, item := range slice {
+		if !seen[item] {
+			seen[item] = true
+			result = append(result, item)
+		}
+	}
+
+	return result
 }

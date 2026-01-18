cc565058b6e31a64bd00155ea3bcf0477d38f92a
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-18 18:30:38 +0000

    Add configuration loader with YAML parsing and environment variable support

--- part9-transfer_1-Prototype/config_loader.go
+++ part9-transfer_1-Prototype/config_loader.go
@@ -104,4 +104,129 @@ func (c *AppConfig) Validate() error {
         return fmt.Errorf("server port must be between 1 and 65535")
     }
     return nil
+}package config
+
+import (
+    "fmt"
+    "os"
+    "strings"
+
+    "gopkg.in/yaml.v2"
+)
+
+type DatabaseConfig struct {
+    Host     string `yaml:"host" env:"DB_HOST"`
+    Port     int    `yaml:"port" env:"DB_PORT"`
+    Username string `yaml:"username" env:"DB_USER"`
+    Password string `yaml:"password" env:"DB_PASS"`
+    Name     string `yaml:"name" env:"DB_NAME"`
+}
+
+type ServerConfig struct {
+    Port         int    `yaml:"port" env:"SERVER_PORT"`
+    Debug        bool   `yaml:"debug" env:"SERVER_DEBUG"`
+    LogLevel     string `yaml:"log_level" env:"LOG_LEVEL"`
+    ReadTimeout  int    `yaml:"read_timeout" env:"READ_TIMEOUT"`
+    WriteTimeout int    `yaml:"write_timeout" env:"WRITE_TIMEOUT"`
+}
+
+type AppConfig struct {
+    Database DatabaseConfig `yaml:"database"`
+    Server   ServerConfig   `yaml:"server"`
+    Features []string       `yaml:"features"`
+}
+
+func LoadConfig(configPath string) (*AppConfig, error) {
+    data, err := os.ReadFile(configPath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    overrideFromEnv(&config)
+
+    return &config, nil
+}
+
+func overrideFromEnv(config *AppConfig) {
+    overrideStruct(config, "")
+}
+
+func overrideStruct(s interface{}, prefix string) {
+    val := reflect.ValueOf(s).Elem()
+    typ := val.Type()
+
+    for i := 0; i < val.NumField(); i++ {
+        field := val.Field(i)
+        fieldType := typ.Field(i)
+
+        envTag := fieldType.Tag.Get("env")
+        yamlTag := fieldType.Tag.Get("yaml")
+
+        if envTag == "" && yamlTag == "" {
+            if field.Kind() == reflect.Struct {
+                nestedPrefix := prefix
+                if yamlTag != "" {
+                    nestedPrefix = strings.TrimSuffix(prefix+"_"+strings.ToUpper(yamlTag), "_")
+                }
+                overrideStruct(field.Addr().Interface(), nestedPrefix)
+            }
+            continue
+        }
+
+        if envTag == "" {
+            envTag = strings.ToUpper(yamlTag)
+        }
+
+        if prefix != "" {
+            envTag = prefix + "_" + envTag
+        }
+
+        envValue := os.Getenv(envTag)
+        if envValue == "" {
+            continue
+        }
+
+        switch field.Kind() {
+        case reflect.String:
+            field.SetString(envValue)
+        case reflect.Int:
+            if intVal, err := strconv.Atoi(envValue); err == nil {
+                field.SetInt(int64(intVal))
+            }
+        case reflect.Bool:
+            if boolVal, err := strconv.ParseBool(envValue); err == nil {
+                field.SetBool(boolVal)
+            }
+        case reflect.Slice:
+            if field.Type().Elem().Kind() == reflect.String {
+                items := strings.Split(envValue, ",")
+                slice := reflect.MakeSlice(field.Type(), len(items), len(items))
+                for j, item := range items {
+                    slice.Index(j).SetString(strings.TrimSpace(item))
+                }
+                field.Set(slice)
+            }
+        }
+    }
+}
+
+func ValidateConfig(config *AppConfig) error {
+    if config.Database.Host == "" {
+        return fmt.Errorf("database host is required")
+    }
+    if config.Database.Port == 0 {
+        return fmt.Errorf("database port is required")
+    }
+    if config.Server.Port == 0 {
+        config.Server.Port = 8080
+    }
+    if config.Server.LogLevel == "" {
+        config.Server.LogLevel = "info"
+    }
+    return nil
 }

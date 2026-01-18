1ce5652d577cec647e1591f1f16c98931ed17118
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-18 00:17:00 +0000

    Add user activity logging with timestamp and action type

--- part5-cli-Prototype/BLC/user_activity_logger.go
+++ part5-cli-Prototype/BLC/user_activity_logger.go
@@ -93,4 +93,49 @@ type responseWriter struct {
 func (rw *responseWriter) WriteHeader(code int) {
 	rw.statusCode = code
 	rw.ResponseWriter.WriteHeader(code)
+}package main
+
+import (
+    "encoding/json"
+    "log"
+    "os"
+    "time"
+)
+
+type ActivityLog struct {
+    Timestamp time.Time `json:"timestamp"`
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Details   string    `json:"details,omitempty"`
+}
+
+func logActivity(userID, action, details string) error {
+    logEntry := ActivityLog{
+        Timestamp: time.Now().UTC(),
+        UserID:    userID,
+        Action:    action,
+        Details:   details,
+    }
+
+    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    encoder := json.NewEncoder(file)
+    encoder.SetIndent("", "  ")
+    return encoder.Encode(logEntry)
+}
+
+func main() {
+    err := logActivity("user123", "login", "Successful authentication")
+    if err != nil {
+        log.Printf("Failed to log activity: %v", err)
+    }
+
+    err = logActivity("user456", "file_upload", "Uploaded profile picture")
+    if err != nil {
+        log.Printf("Failed to log activity: %v", err)
+    }
 }

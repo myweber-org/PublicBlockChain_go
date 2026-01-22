2fc76962ace73b41543951e0f485f0e6b729ffee
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-22 20:00:41 +0000

    Add user activity logging middleware with request details and timestamp

--- part2-ProofOfWork-Prototype/user_activity_logger.go
+++ part2-ProofOfWork-Prototype/user_activity_logger.go
@@ -1,108 +1,35 @@
 package middleware
 
 import (
-	"log"
-	"net/http"
-	"sync"
-	"time"
+    "log"
+    "net/http"
+    "time"
 )
 
-type ActivityLogger struct {
-	mu          sync.RWMutex
-	userLimits  map[string]time.Time
-	rateLimit   time.Duration
-	logFilePath string
+type responseWriter struct {
+    http.ResponseWriter
+    statusCode int
 }
 
-func NewActivityLogger(limit time.Duration, logFile string) *ActivityLogger {
-	return &ActivityLogger{
-		userLimits:  make(map[string]time.Time),
-		rateLimit:   limit,
-		logFilePath: logFile,
-	}
+func (rw *responseWriter) WriteHeader(code int) {
+    rw.statusCode = code
+    rw.ResponseWriter.WriteHeader(code)
 }
 
-func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		userID := r.Header.Get("X-User-ID")
-		if userID == "" {
-			userID = "anonymous"
-		}
-
-		now := time.Now()
-		al.mu.RLock()
-		lastLog, exists := al.userLimits[userID]
-		al.mu.RUnlock()
-
-		if exists && now.Sub(lastLog) < al.rateLimit {
-			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
-			return
-		}
-
-		al.mu.Lock()
-		al.userLimits[userID] = now
-		al.mu.Unlock()
-
-		logEntry := struct {
-			Timestamp time.Time `json:"timestamp"`
-			UserID    string    `json:"user_id"`
-			Method    string    `json:"method"`
-			Path      string    `json:"path"`
-			IP        string    `json:"ip"`
-		}{
-			Timestamp: now,
-			UserID:    userID,
-			Method:    r.Method,
-			Path:      r.URL.Path,
-			IP:        r.RemoteAddr,
-		}
-
-		log.Printf("Activity: %s %s by %s from %s", logEntry.Method, logEntry.Path, logEntry.UserID, logEntry.IP)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (al *ActivityLogger) CleanupOldEntries() {
-	ticker := time.NewTicker(time.Hour)
-	go func() {
-		for range ticker.C {
-			al.mu.Lock()
-			cutoff := time.Now().Add(-24 * time.Hour)
-			for userID, lastLog := range al.userLimits {
-				if lastLog.Before(cutoff) {
-					delete(al.userLimits, userID)
-				}
-			}
-			al.mu.Unlock()
-		}
-	}()
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
-
-	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | UserAgent: %s",
-		r.Method,
-		r.URL.Path,
-		duration,
-		r.UserAgent(),
-	)
+func ActivityLogger(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+        rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+        
+        next.ServeHTTP(rw, r)
+        
+        duration := time.Since(start)
+        log.Printf("%s %s %d %s %s",
+            r.Method,
+            r.URL.Path,
+            rw.statusCode,
+            duration,
+            r.RemoteAddr,
+        )
+    })
 }

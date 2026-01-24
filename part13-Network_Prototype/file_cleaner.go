0a965985203035556bb90f49bd62292049dd2faf
InkspillVerse <inkspill.verse@outlook.com>
2026-01-24 23:21:40 +0000

    Add utility to remove temporary files older than 7 days

--- part13-Network_Prototype/file_cleaner.go
+++ part13-Network_Prototype/file_cleaner.go
@@ -22,4 +22,34 @@ func main() {
         }
         return nil
     })
+}package main
+
+import (
+    "os"
+    "path/filepath"
+    "time"
+)
+
+func cleanOldFiles(dir string, maxAge time.Duration) error {
+    cutoff := time.Now().Add(-maxAge)
+    return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if info.IsDir() {
+            return nil
+        }
+        if info.ModTime().Before(cutoff) {
+            return os.Remove(path)
+        }
+        return nil
+    })
+}
+
+func main() {
+    tempDir := os.TempDir()
+    err := cleanOldFiles(tempDir, 7*24*time.Hour)
+    if err != nil {
+        panic(err)
+    }
 }

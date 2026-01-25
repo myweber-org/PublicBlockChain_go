ddfdc28b13f9dac0198ef94dda6fa402f6e3c48f
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-25 20:07:35 +0000

    Add user activity logging middleware for API endpoints

--- part5-cli-Prototype/BLC/user_activity_logger.go
+++ part5-cli-Prototype/BLC/user_activity_logger.go
@@ -6,41 +6,24 @@ import (
 	"time"
 )
 
-type ActivityLog struct {
-	UserID    string
-	IPAddress string
-	Method    string
-	Path      string
-	Timestamp time.Time
+type ActivityLogger struct {
+	handler http.Handler
 }
 
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		
-		userID := "anonymous"
-		if authHeader := r.Header.Get("Authorization"); authHeader != "" {
-			userID = extractUserID(authHeader)
-		}
-
-		activity := ActivityLog{
-			UserID:    userID,
-			IPAddress: r.RemoteAddr,
-			Method:    r.Method,
-			Path:      r.URL.Path,
-			Timestamp: start,
-		}
-
-		log.Printf("Activity: %s %s by %s from %s", 
-			activity.Method, 
-			activity.Path, 
-			activity.UserID, 
-			activity.IPAddress)
-
-		next.ServeHTTP(w, r)
-	})
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func extractUserID(token string) string {
-	return "user_" + token[:8]
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	
+	al.handler.ServeHTTP(w, r)
+	
+	duration := time.Since(start)
+	
+	log.Printf("Activity: %s %s from %s took %v", 
+		r.Method, 
+		r.URL.Path, 
+		r.RemoteAddr, 
+		duration)
 }

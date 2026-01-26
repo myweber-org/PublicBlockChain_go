b4342a37e84cf341cf012e635841e85cd8a72c7d
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-01-26 01:43:42 +0000

    Add user activity logging middleware with request details and timestamp

--- part11-transaction_1_Prototype/user_activity_logger.go
+++ part11-transaction_1_Prototype/user_activity_logger.go
@@ -6,25 +6,32 @@ import (
 	"time"
 )
 
-type ActivityLogger struct {
-	handler http.Handler
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+		next.ServeHTTP(rw, r)
+
+		duration := time.Since(start)
+		log.Printf(
+			"Method: %s | Path: %s | Status: %d | Duration: %v | RemoteAddr: %s | UserAgent: %s",
+			r.Method,
+			r.URL.Path,
+			rw.statusCode,
+			duration,
+			r.RemoteAddr,
+			r.Header.Get("User-Agent"),
+		)
+	})
 }

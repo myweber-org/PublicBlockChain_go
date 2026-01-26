35acd27bcff9a25492006605f582a7d007a828e7
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-26 00:07:17 +0000

    Add user activity logging middleware to track API usage

--- part4-DataPersistence-Prototype/BLC/user_activity_logger.go
+++ part4-DataPersistence-Prototype/BLC/user_activity_logger.go
@@ -1,46 +1,29 @@
-package main
+package middleware
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"os"
+	"net/http"
 	"time"
 )
 
-type ActivityLog struct {
-	Timestamp time.Time `json:"timestamp"`
-	UserID    string    `json:"user_id"`
-	Action    string    `json:"action"`
-	Details   string    `json:"details"`
+type ActivityLogger struct {
+	handler http.Handler
 }
 
-func logActivity(userID, action, details string) {
-	activity := ActivityLog{
-		Timestamp: time.Now(),
-		UserID:    userID,
-		Action:    action,
-		Details:   details,
-	}
-
-	file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Printf("Failed to open log file: %v", err)
-		return
-	}
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	if err := encoder.Encode(activity); err != nil {
-		log.Printf("Failed to encode activity: %v", err)
-		return
-	}
-
-	fmt.Printf("Logged activity: %s performed %s\n", userID, action)
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
 }
 
-func main() {
-	logActivity("user123", "login", "User logged in from web browser")
-	logActivity("user456", "file_upload", "Uploaded document.pdf")
-	logActivity("user123", "logout", "Session expired after 30 minutes")
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
+
+	log.Printf(
+		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
+		r.Method,
+		r.URL.Path,
+		duration,
+		time.Now().Format(time.RFC3339),
+	)
 }

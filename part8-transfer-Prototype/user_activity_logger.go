8011931ccd51f2f949770766a6cc353086405465
InkspillVerse <inkspill.verse@outlook.com>
2026-01-26 01:16:02 +0000

    Add user activity logging middleware for tracking API usage

--- part8-transfer-Prototype/user_activity_logger.go
+++ part8-transfer-Prototype/user_activity_logger.go
@@ -7,24 +7,47 @@ import (
 )
 
 type ActivityLogger struct {
-	handler http.Handler
+	Logger *log.Logger
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func NewActivityLogger(logger *log.Logger) *ActivityLogger {
+	return &ActivityLogger{Logger: logger}
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
+func (al *ActivityLogger) LogActivity(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		clientIP := r.RemoteAddr
+		method := r.Method
+		path := r.URL.Path
+
+		recorder := &responseRecorder{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+
+		next.ServeHTTP(recorder, r)
+
+		duration := time.Since(start)
+		status := recorder.statusCode
+
+		al.Logger.Printf(
+			"IP: %s | Method: %s | Path: %s | Status: %d | Duration: %v",
+			clientIP,
+			method,
+			path,
+			status,
+			duration,
+		)
+	})
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }

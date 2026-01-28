c02ceb768241b2e78f8e12cf11a3d71e16175637
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-28 00:53:55 +0000

    Add data processing utility for cleaning and validating input strings

--- part2-ProofOfWork-Prototype/data_processor.go
+++ part2-ProofOfWork-Prototype/data_processor.go
@@ -142,3 +142,41 @@ func ExtractDomain(email string) (string, bool) {
 	
 	return parts[1], true
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	allowedPattern *regexp.Regexp
+}
+
+func NewDataProcessor(pattern string) (*DataProcessor, error) {
+	compiled, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, err
+	}
+	return &DataProcessor{allowedPattern: compiled}, nil
+}
+
+func (dp *DataProcessor) CleanInput(input string) string {
+	trimmed := strings.TrimSpace(input)
+	return dp.allowedPattern.FindString(trimmed)
+}
+
+func (dp *DataProcessor) Validate(input string) bool {
+	return dp.allowedPattern.MatchString(input)
+}
+
+func (dp *DataProcessor) ProcessBatch(inputs []string) []string {
+	var results []string
+	for _, item := range inputs {
+		cleaned := dp.CleanInput(item)
+		if cleaned != "" {
+			results = append(results, cleaned)
+		}
+	}
+	return results
+}

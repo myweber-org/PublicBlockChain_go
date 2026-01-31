fafaf67d5ad878220590a45fdf42a300ed2799b0
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-01-31 20:06:41 +0000

    Add configuration loader with YAML parsing support

--- part6-cli1-Prototype/config_loader.go
+++ part6-cli1-Prototype/config_loader.go
@@ -276,4 +276,36 @@ func getEnvIntOrDefault(key string, defaultValue int) int {
 		}
 	}
 	return defaultValue
+}package config
+
+import (
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+    } `yaml:"database"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+    data, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+
+    var config Config
+    err = yaml.Unmarshal(data, &config)
+    if err != nil {
+        return nil, err
+    }
+
+    return &config, nil
 }

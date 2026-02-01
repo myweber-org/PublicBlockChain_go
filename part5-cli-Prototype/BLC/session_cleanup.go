2b2f23e49c8f5fed1039003a1ad7a5887a5fe132
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-01 01:27:50 +0000

    Add session cleanup job to remove expired sessions from the database

--- part5-cli-Prototype/BLC/session_cleanup.go
+++ part5-cli-Prototype/BLC/session_cleanup.go
@@ -1,132 +1,48 @@
 package main
 
 import (
-	"context"
-	"log"
-	"time"
-
-	"github.com/redis/go-redis/v9"
+    "context"
+    "database/sql"
+    "log"
+    "time"
 )
 
-const (
-	sessionKeyPattern = "session:*"
-	batchSize         = 100
-)
+const cleanupInterval = 1 * time.Hour
+const sessionTTL = 24 * time.Hour
 
-func cleanupExpiredSessions(rdb *redis.Client) error {
-	ctx := context.Background()
-	var cursor uint64
-	var keys []string
-	var err error
+func cleanupExpiredSessions(db *sql.DB) {
+    ctx := context.Background()
+    query := `DELETE FROM user_sessions WHERE last_activity < $1`
+    cutoff := time.Now().Add(-sessionTTL)
 
-	for {
-		keys, cursor, err = rdb.Scan(ctx, cursor, sessionKeyPattern, batchSize).Result()
-		if err != nil {
-			return err
-		}
+    result, err := db.ExecContext(ctx, query, cutoff)
+    if err != nil {
+        log.Printf("Failed to clean up sessions: %v", err)
+        return
+    }
 
-		for _, key := range keys {
-			ttl, err := rdb.TTL(ctx, key).Result()
-			if err != nil {
-				log.Printf("Failed to get TTL for key %s: %v", key, err)
-				continue
-			}
-			if ttl < 0 {
-				if err := rdb.Del(ctx, key).Err(); err != nil {
-					log.Printf("Failed to delete expired key %s: %v", key, err)
-				} else {
-					log.Printf("Deleted expired session: %s", key)
-				}
-			}
-		}
+    rowsAffected, _ := result.RowsAffected()
+    log.Printf("Cleaned up %d expired sessions", rowsAffected)
+}
 
-		if cursor == 0 {
-			break
-		}
-	}
-	return nil
+func startSessionCleanupJob(db *sql.DB) {
+    ticker := time.NewTicker(cleanupInterval)
+    defer ticker.Stop()
+
+    for {
+        select {
+        case <-ticker.C:
+            cleanupExpiredSessions(db)
+        }
+    }
 }
 
 func main() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
+    db, err := sql.Open("postgres", "postgresql://localhost/sessions")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
 
-	ticker := time.NewTicker(24 * time.Hour)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		log.Println("Starting session cleanup...")
-		if err := cleanupExpiredSessions(rdb); err != nil {
-			log.Printf("Session cleanup failed: %v", err)
-		} else {
-			log.Println("Session cleanup completed")
-		}
-	}
-}
-package main
-
-import (
-	"log"
-	"time"
-)
-
-type Session struct {
-	ID        string
-	UserID    string
-	ExpiresAt time.Time
-}
-
-type SessionStore interface {
-	GetAllSessions() ([]Session, error)
-	DeleteSession(id string) error
-}
-
-type DBSessionStore struct{}
-
-func (s *DBSessionStore) GetAllSessions() ([]Session, error) {
-	return []Session{}, nil
-}
-
-func (s *DBSessionStore) DeleteSession(id string) error {
-	return nil
-}
-
-func cleanupExpiredSessions(store SessionStore) error {
-	sessions, err := store.GetAllSessions()
-	if err != nil {
-		return err
-	}
-
-	now := time.Now()
-	for _, session := range sessions {
-		if session.ExpiresAt.Before(now) {
-			if err := store.DeleteSession(session.ID); err != nil {
-				log.Printf("Failed to delete session %s: %v", session.ID, err)
-			} else {
-				log.Printf("Deleted expired session: %s", session.ID)
-			}
-		}
-	}
-	return nil
-}
-
-func scheduleCleanup(store SessionStore, interval time.Duration) {
-	ticker := time.NewTicker(interval)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		if err := cleanupExpiredSessions(store); err != nil {
-			log.Printf("Session cleanup failed: %v", err)
-		}
-	}
-}
-
-func main() {
-	store := &DBSessionStore{}
-	go scheduleCleanup(store, 24*time.Hour)
-
-	select {}
+    startSessionCleanupJob(db)
 }

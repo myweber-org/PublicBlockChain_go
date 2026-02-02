3f57c9fff97e480a162ed89fb4abbc73697d4b30
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-02 15:31:37 +0000

    Add utility to remove duplicate lines from a text file

---
+++ part7-transaction-Prototype/BLC/file_cleaner.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_cleaner <input_file>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := inputFile + ".cleaned"
+
+	lines, err := readLines(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	uniqueLines := removeDuplicates(lines)
+
+	err = writeLines(outputFile, uniqueLines)
+	if err != nil {
+		fmt.Printf("Error writing file: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Cleaned file saved as: %s\n", outputFile)
+}
+
+func readLines(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func removeDuplicates(lines []string) []string {
+	seen := make(map[string]bool)
+	var result []string
+	for _, line := range lines {
+		trimmed := strings.TrimSpace(line)
+		if trimmed == "" {
+			result = append(result, "")
+			continue
+		}
+		if !seen[trimmed] {
+			seen[trimmed] = true
+			result = append(result, line)
+		}
+	}
+	return result
+}
+
+func writeLines(filename string, lines []string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for _, line := range lines {
+		_, err := writer.WriteString(line + "\n")
+		if err != nil {
+			return err
+		}
+	}
+	return writer.Flush()
+}

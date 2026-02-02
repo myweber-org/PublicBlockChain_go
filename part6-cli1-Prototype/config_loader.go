24ddda6b93fd58c345ce4c80fec3782dc05c1ad4
FelixMakes <felix.makes@hotmail.com>
2026-02-02 15:08:09 +0000

    Add configuration loader with YAML support and environment variable overrides

--- part6-cli1-Prototype/config_loader.go
+++ part6-cli1-Prototype/config_loader.go
@@ -308,4 +308,107 @@ func LoadConfig(filename string) (*Config, error) {
     }
 
     return &config, nil
+}package config
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Output string `yaml:"output" env:"LOG_OUTPUT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	if configPath == "" {
+		configPath = "config.yaml"
+	}
+
+	absPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	if err := overrideFromEnv(&config); err != nil {
+		return nil, err
+	}
+
+	if err := validateConfig(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func overrideFromEnv(config *Config) error {
+	envVars := map[string]string{
+		"SERVER_HOST":     &config.Server.Host,
+		"SERVER_PORT":     stringPtrFromInt(config.Server.Port),
+		"DB_HOST":         &config.Database.Host,
+		"DB_PORT":         stringPtrFromInt(config.Database.Port),
+		"DB_NAME":         &config.Database.Name,
+		"DB_USER":         &config.Database.User,
+		"DB_PASSWORD":     &config.Database.Password,
+		"LOG_LEVEL":       &config.Logging.Level,
+		"LOG_OUTPUT":      &config.Logging.Output,
+	}
+
+	for envVar, fieldPtr := range envVars {
+		if val, exists := os.LookupEnv(envVar); exists && val != "" {
+			*fieldPtr = val
+		}
+	}
+
+	return nil
+}
+
+func stringPtrFromInt(i int) *string {
+	s := fmt.Sprintf("%d", i)
+	return &s
+}
+
+func validateConfig(config *Config) error {
+	if config.Server.Host == "" {
+		return errors.New("server host cannot be empty")
+	}
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	if config.Database.Name == "" {
+		return errors.New("database name cannot be empty")
+	}
+	if config.Logging.Level == "" {
+		config.Logging.Level = "info"
+	}
+	if config.Logging.Output == "" {
+		config.Logging.Output = "stdout"
+	}
+	return nil
 }

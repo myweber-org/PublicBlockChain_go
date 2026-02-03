0549f009853d5878ec075f6f345e942a4e174b99
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-03 19:22:31 +0000

    Add user activity logging with timestamp and action type

--- part12-MerkleTree_Prototype/BLC/user_activity_logger.go
+++ part12-MerkleTree_Prototype/BLC/user_activity_logger.go
@@ -82,3 +82,62 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		duration,
 	)
 }
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "os"
+    "time"
+)
+
+type UserActivity struct {
+    UserID    string    `json:"user_id"`
+    Action    string    `json:"action"`
+    Timestamp time.Time `json:"timestamp"`
+    Details   string    `json:"details,omitempty"`
+}
+
+func logActivity(userID, action, details string) error {
+    activity := UserActivity{
+        UserID:    userID,
+        Action:    action,
+        Timestamp: time.Now().UTC(),
+        Details:   details,
+    }
+
+    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    encoder := json.NewEncoder(file)
+    encoder.SetIndent("", "  ")
+    if err := encoder.Encode(activity); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func main() {
+    activities := []struct {
+        userID  string
+        action  string
+        details string
+    }{
+        {"user_001", "login", "Successful authentication"},
+        {"user_002", "purchase", "Order ID: INV-78945"},
+        {"user_001", "logout", "Session duration: 25m"},
+    }
+
+    for _, act := range activities {
+        if err := logActivity(act.userID, act.action, act.details); err != nil {
+            log.Printf("Failed to log activity: %v", err)
+        } else {
+            fmt.Printf("Logged %s for user %s\n", act.action, act.userID)
+        }
+    }
+}

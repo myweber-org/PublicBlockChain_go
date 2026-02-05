643ed4fc65bbf8ee799e18775c4f45622f21d285
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-05 16:01:18 +0000

    Add log rotation with size-based file splitting and compression

--- part4-DataPersistence-Prototype/BLC/log_rotator.go
+++ part4-DataPersistence-Prototype/BLC/log_rotator.go
@@ -2,147 +2,148 @@
 package main
 
 import (
-    "compress/gzip"
-    "fmt"
-    "io"
-    "os"
-    "path/filepath"
-    "sync"
-    "time"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
 )
 
 type RotatingLogger struct {
-    mu           sync.Mutex
-    currentFile  *os.File
-    basePath     string
-    maxSize      int64
-    currentSize  int64
-    rotationCount int
+	mu          sync.Mutex
+	currentFile *os.File
+	basePath    string
+	maxSize     int64
+	fileSize    int64
+	backupCount int
 }
 
-func NewRotatingLogger(basePath string, maxSizeMB int) (*RotatingLogger, error) {
-    maxSize := int64(maxSizeMB) * 1024 * 1024
+func NewRotatingLogger(basePath string, maxSizeMB int, backupCount int) (*RotatingLogger, error) {
+	maxSize := int64(maxSizeMB) * 1024 * 1024
+	logger := &RotatingLogger{
+		basePath:    basePath,
+		maxSize:     maxSize,
+		backupCount: backupCount,
+	}
 
-    file, err := os.OpenFile(basePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return nil, err
-    }
+	if err := logger.openCurrentFile(); err != nil {
+		return nil, err
+	}
 
-    info, err := file.Stat()
-    if err != nil {
-        file.Close()
-        return nil, err
-    }
-
-    return &RotatingLogger{
-        currentFile:  file,
-        basePath:     basePath,
-        maxSize:      maxSize,
-        currentSize:  info.Size(),
-        rotationCount: 0,
-    }, nil
+	return logger, nil
 }
 
-func (rl *RotatingLogger) Write(p []byte) (int, error) {
-    rl.mu.Lock()
-    defer rl.mu.Unlock()
+func (l *RotatingLogger) openCurrentFile() error {
+	dir := filepath.Dir(l.basePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
 
-    if rl.currentSize+int64(len(p)) > rl.maxSize {
-        if err := rl.rotate(); err != nil {
-            return 0, err
-        }
-    }
+	file, err := os.OpenFile(l.basePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
 
-    n, err := rl.currentFile.Write(p)
-    if err == nil {
-        rl.currentSize += int64(n)
-    }
-    return n, err
+	stat, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	l.currentFile = file
+	l.fileSize = stat.Size()
+	return nil
 }
 
-func (rl *RotatingLogger) rotate() error {
-    if err := rl.currentFile.Close(); err != nil {
-        return err
-    }
+func (l *RotatingLogger) Write(p []byte) (int, error) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
 
-    timestamp := time.Now().Format("20060102_150405")
-    archivedPath := fmt.Sprintf("%s.%s.gz", rl.basePath, timestamp)
+	if l.fileSize+int64(len(p)) > l.maxSize {
+		if err := l.rotate(); err != nil {
+			return 0, err
+		}
+	}
 
-    if err := compressFile(rl.basePath, archivedPath); err != nil {
-        return err
-    }
+	n, err := l.currentFile.Write(p)
+	if err == nil {
+		l.fileSize += int64(n)
+	}
+	return n, err
+}
 
-    if err := os.Remove(rl.basePath); err != nil {
-        return err
-    }
+func (l *RotatingLogger) rotate() error {
+	if err := l.currentFile.Close(); err != nil {
+		return err
+	}
 
-    file, err := os.OpenFile(rl.basePath, os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return err
-    }
+	timestamp := time.Now().Format("20060102_150405")
+	backupPath := fmt.Sprintf("%s.%s.gz", l.basePath, timestamp)
 
-    rl.currentFile = file
-    rl.currentSize = 0
-    rl.rotationCount++
+	if err := compressFile(l.basePath, backupPath); err != nil {
+		return err
+	}
 
-    if rl.rotationCount > 10 {
-        rl.cleanOldArchives()
-    }
+	if err := os.Remove(l.basePath); err != nil {
+		return err
+	}
 
-    return nil
+	if err := l.cleanOldBackups(); err != nil {
+		return err
+	}
+
+	return l.openCurrentFile()
 }
 
 func compressFile(source, target string) error {
-    srcFile, err := os.Open(source)
-    if err != nil {
-        return err
-    }
-    defer srcFile.Close()
+	srcFile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
 
-    dstFile, err := os.Create(target)
-    if err != nil {
-        return err
-    }
-    defer dstFile.Close()
+	dstFile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
 
-    gzWriter := gzip.NewWriter(dstFile)
-    defer gzWriter.Close()
+	gzWriter := gzip.NewWriter(dstFile)
+	defer gzWriter.Close()
 
-    _, err = io.Copy(gzWriter, srcFile)
-    return err
+	_, err = io.Copy(gzWriter, srcFile)
+	return err
 }
 
-func (rl *RotatingLogger) cleanOldArchives() {
-    pattern := rl.basePath + ".*.gz"
-    matches, err := filepath.Glob(pattern)
-    if err != nil {
-        return
-    }
+func (l *RotatingLogger) cleanOldBackups() error {
+	pattern := l.basePath + ".*.gz"
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return err
+	}
 
-    if len(matches) > 10 {
-        filesToDelete := matches[:len(matches)-10]
-        for _, file := range filesToDelete {
-            os.Remove(file)
-        }
-    }
+	if len(matches) <= l.backupCount {
+		return nil
+	}
+
+	oldestFirst := matches[:len(matches)-l.backupCount]
+	for _, file := range oldestFirst {
+		if err := os.Remove(file); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
-func (rl *RotatingLogger) Close() error {
-    rl.mu.Lock()
-    defer rl.mu.Unlock()
-    return rl.currentFile.Close()
-}
+func (l *RotatingLogger) Close() error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
 
-func main() {
-    logger, err := NewRotatingLogger("app.log", 10)
-    if err != nil {
-        panic(err)
-    }
-    defer logger.Close()
-
-    for i := 0; i < 1000; i++ {
-        message := fmt.Sprintf("Log entry %d: Application event occurred at %v\n", i, time.Now())
-        logger.Write([]byte(message))
-        time.Sleep(10 * time.Millisecond)
-    }
+	if l.currentFile != nil {
+		return l.currentFile.Close()
+	}
+	return nil
 }

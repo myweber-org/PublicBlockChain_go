3fb30cc48c5c647ae1c049449a203942ec75c538
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-06 22:42:05 +0000

    Add configuration file loader with YAML parsing support

--- part5-cli-Prototype/BLC/config_loader.go
+++ part5-cli-Prototype/BLC/config_loader.go
@@ -349,4 +349,65 @@ func getEnv(key, defaultValue string) string {
         return defaultValue
     }
     return value
+}package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Port    int    `yaml:"port"`
+		Host    string `yaml:"host"`
+		Timeout int    `yaml:"timeout"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+		Name     string `yaml:"name"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level"`
+		Output string `yaml:"output"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func ValidateConfig(config *Config) bool {
+	if config.Server.Port <= 0 || config.Server.Port > 65535 {
+		log.Printf("Invalid server port: %d", config.Server.Port)
+		return false
+	}
+
+	if config.Database.Host == "" {
+		log.Print("Database host cannot be empty")
+		return false
+	}
+
+	if config.Logging.Level != "debug" && config.Logging.Level != "info" &&
+		config.Logging.Level != "warn" && config.Logging.Level != "error" {
+		log.Printf("Invalid logging level: %s", config.Logging.Level)
+		return false
+	}
+
+	return true
 }

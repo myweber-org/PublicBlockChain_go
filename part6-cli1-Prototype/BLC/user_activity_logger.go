cc19d25532232dbe512b24a5a2f59e8dc1dd7895
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-06 19:05:22 +0000

    Add user activity logging middleware for tracking API usage

--- part6-cli1-Prototype/BLC/user_activity_logger.go
+++ part6-cli1-Prototype/BLC/user_activity_logger.go
@@ -1,3 +1,4 @@
+
 package middleware
 
 import (
@@ -15,63 +16,16 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 }
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	startTime := time.Now()
-	recorder := &responseRecorder{
-		ResponseWriter: w,
-		statusCode:     http.StatusOK,
-	}
-
-	al.handler.ServeHTTP(recorder, r)
-
-	duration := time.Since(startTime)
-	log.Printf(
-		"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
-		r.Method,
-		r.URL.Path,
-		recorder.statusCode,
-		duration,
-		r.UserAgent(),
-	)
-}
-
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	
 	recorder := &responseRecorder{
 		ResponseWriter: w,
 		statusCode:     http.StatusOK,
 	}
-	
+
 	al.handler.ServeHTTP(recorder, r)
-	
+
 	duration := time.Since(start)
-	
-	log.Printf(
-		"%s %s %d %s %s",
+	log.Printf("%s %s %d %v %s",
 		r.Method,
 		r.URL.Path,
 		recorder.statusCode,

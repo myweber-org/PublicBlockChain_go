b2cfc49f16ca15659b501ff297ea6af6d9bc34cc
FelixMakes <felix.makes@hotmail.com>
2026-02-06 16:48:13 +0000

    Add data processing utility for cleaning and validating input strings

--- part10-address_Prototype/data_processor.go
+++ part10-address_Prototype/data_processor.go
@@ -47,3 +47,47 @@ func main() {
 
 	fmt.Printf("Processed: %+v\n", result)
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+	emailRegex      *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+		emailRegex:      regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	return dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+}
+
+func (dp *DataProcessor) ValidateEmail(email string) bool {
+	return dp.emailRegex.MatchString(email)
+}
+
+func (dp *DataProcessor) NormalizeEmail(email string) (string, bool) {
+	cleaned := dp.CleanString(email)
+	normalized := strings.ToLower(cleaned)
+	return normalized, dp.ValidateEmail(normalized)
+}
+
+func (dp *DataProcessor) ProcessInputList(inputs []string) []string {
+	var processed []string
+	for _, input := range inputs {
+		cleaned := dp.CleanString(input)
+		if cleaned != "" {
+			processed = append(processed, cleaned)
+		}
+	}
+	return processed
+}

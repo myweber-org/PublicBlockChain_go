cfd1677208f28dd9d7c0f1cbf4ea6151854b71d6
heyQuim <hey.quim@hotmail.com>
2026-02-06 03:24:48 +0000

    Add file processing utility with concurrency support

--- part7-transaction-Prototype/BLC/file_processor.go
+++ part7-transaction-Prototype/BLC/file_processor.go
@@ -92,4 +92,112 @@ func main() {
 
 	results := processor.GetResults()
 	fmt.Printf("\nTotal files processed: %d\n", len(results))
+}package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "path/filepath"
+    "sync"
+)
+
+type FileProcessor struct {
+    inputDir  string
+    outputDir string
+    wg        sync.WaitGroup
+}
+
+func NewFileProcessor(input, output string) *FileProcessor {
+    return &FileProcessor{
+        inputDir:  input,
+        outputDir: output,
+    }
+}
+
+func (fp *FileProcessor) ProcessFile(filename string) error {
+    defer fp.wg.Done()
+
+    inputPath := filepath.Join(fp.inputDir, filename)
+    outputPath := filepath.Join(fp.outputDir, "processed_"+filename)
+
+    inputFile, err := os.Open(inputPath)
+    if err != nil {
+        return fmt.Errorf("cannot open input file: %w", err)
+    }
+    defer inputFile.Close()
+
+    outputFile, err := os.Create(outputPath)
+    if err != nil {
+        return fmt.Errorf("cannot create output file: %w", err)
+    }
+    defer outputFile.Close()
+
+    scanner := bufio.NewScanner(inputFile)
+    writer := bufio.NewWriter(outputFile)
+
+    for scanner.Scan() {
+        line := scanner.Text()
+        processedLine := transformLine(line)
+        _, err := writer.WriteString(processedLine + "\n")
+        if err != nil {
+            return fmt.Errorf("write error: %w", err)
+        }
+    }
+
+    if err := scanner.Err(); err != nil {
+        return fmt.Errorf("scan error: %w", err)
+    }
+
+    return writer.Flush()
+}
+
+func transformLine(line string) string {
+    return "PROCESSED: " + line
+}
+
+func (fp *FileProcessor) ProcessAll() []error {
+    entries, err := os.ReadDir(fp.inputDir)
+    if err != nil {
+        return []error{fmt.Errorf("cannot read directory: %w", err)}
+    }
+
+    var errors []error
+    errorChan := make(chan error, len(entries))
+
+    for _, entry := range entries {
+        if entry.IsDir() {
+            continue
+        }
+
+        fp.wg.Add(1)
+        go func(fname string) {
+            if err := fp.ProcessFile(fname); err != nil {
+                errorChan <- err
+            }
+        }(entry.Name())
+    }
+
+    fp.wg.Wait()
+    close(errorChan)
+
+    for err := range errorChan {
+        errors = append(errors, err)
+    }
+
+    return errors
+}
+
+func main() {
+    processor := NewFileProcessor("./input", "./output")
+    errors := processor.ProcessAll()
+
+    if len(errors) > 0 {
+        fmt.Printf("Processing completed with %d errors:\n", len(errors))
+        for _, err := range errors {
+            fmt.Println(err)
+        }
+    } else {
+        fmt.Println("All files processed successfully")
+    }
 }

dad5dd1397b4c56c2f5bfba7bb005e19e6611bd4
FelixMakes <felix.makes@hotmail.com>
2026-02-06 01:07:47 +0000

    Add file encryption utility with AES-256-GCM and key derivation

--- part9-transfer_1-Prototype/file_encryption_utility.go
+++ part9-transfer_1-Prototype/file_encryption_utility.go
@@ -343,4 +343,95 @@ func main() {
 	}
 
 	fmt.Printf("Decrypted: %s\n", decrypted)
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+func deriveKey(passphrase string) []byte {
+	hash := sha256.Sum256([]byte(passphrase))
+	return hash[:]
+}
+
+func encryptData(plaintext []byte, passphrase string) ([]byte, error) {
+	key := deriveKey(passphrase)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return ciphertext, nil
+}
+
+func decryptData(ciphertext []byte, passphrase string) ([]byte, error) {
+	key := deriveKey(passphrase)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func main() {
+	secretMessage := "Confidential data: Operation Midnight"
+	password := "securePass123!"
+
+	fmt.Println("Original message:", secretMessage)
+
+	encrypted, err := encryptData([]byte(secretMessage), password)
+	if err != nil {
+		fmt.Println("Encryption error:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Encrypted (hex):", hex.EncodeToString(encrypted))
+
+	decrypted, err := decryptData(encrypted, password)
+	if err != nil {
+		fmt.Println("Decryption error:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Decrypted message:", string(decrypted))
+
+	if string(decrypted) == secretMessage {
+		fmt.Println("Verification: Encryption/decryption successful")
+	} else {
+		fmt.Println("Verification: Data mismatch")
+	}
 }

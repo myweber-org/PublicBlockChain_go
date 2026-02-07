a6f7b388bd8f941931541fe51a71e0cc5adb39b6
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-07 19:57:32 +0000

    Add data processing function to calculate average from slice of integers

--- part4-DataPersistence-Prototype/BLC/data_processor.go
+++ part4-DataPersistence-Prototype/BLC/data_processor.go
@@ -141,3 +141,25 @@ func IsNumeric(s string) bool {
 	matched, _ := regexp.MatchString(pattern, s)
 	return matched
 }
+package main
+
+import "fmt"
+
+func calculateAverage(numbers []int) float64 {
+    if len(numbers) == 0 {
+        return 0
+    }
+    
+    sum := 0
+    for _, num := range numbers {
+        sum += num
+    }
+    
+    return float64(sum) / float64(len(numbers))
+}
+
+func main() {
+    data := []int{10, 20, 30, 40, 50}
+    avg := calculateAverage(data)
+    fmt.Printf("Average: %.2f\n", avg)
+}

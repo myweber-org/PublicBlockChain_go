720fc696f67c050c0eb3e63f3df7716fc639f424
InkspillVerse <inkspill.verse@outlook.com>
2026-02-07 15:43:53 +0000

    Add data cleaning utility for CSV files with duplicate removal

--- part11-transaction_1_Prototype/BLC/data_cleaner.go
+++ part11-transaction_1_Prototype/BLC/data_cleaner.go
@@ -24,4 +24,71 @@ func main() {
 	cleaned := RemoveDuplicates(data)
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
+}package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: data_cleaner <input.csv>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := strings.TrimSuffix(inputFile, ".csv") + "_cleaned.csv"
+
+	err := removeDuplicates(inputFile, outputFile)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Cleaned data saved to: %s\n", outputFile)
+}
+
+func removeDuplicates(inputPath, outputPath string) error {
+	inFile, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	reader := csv.NewReader(inFile)
+	writer := csv.NewWriter(outFile)
+	defer writer.Flush()
+
+	seen := make(map[string]bool)
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		key := strings.Join(record, "|")
+		if !seen[key] {
+			seen[key] = true
+			err = writer.Write(record)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }

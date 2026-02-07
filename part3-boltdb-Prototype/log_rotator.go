718718e69fb4f201b1c87583104326a1151e5521
InkspillVerse <inkspill.verse@outlook.com>
2026-02-07 00:11:28 +0000

    Implement log rotation with size-based file splitting and compression

--- part3-boltdb-Prototype/log_rotator.go
+++ part3-boltdb-Prototype/log_rotator.go
@@ -245,4 +245,181 @@ func main() {
 	}
 
 	fmt.Println("Log rotation test completed")
+}package main
+
+import (
+    "compress/gzip"
+    "fmt"
+    "io"
+    "os"
+    "path/filepath"
+    "strconv"
+    "strings"
+    "sync"
+    "time"
+)
+
+type RotatingLogger struct {
+    mu            sync.Mutex
+    basePath      string
+    maxSize       int64
+    currentSize   int64
+    currentFile   *os.File
+    sequence      int
+    compressOld   bool
+}
+
+func NewRotatingLogger(basePath string, maxSizeMB int, compress bool) (*RotatingLogger, error) {
+    maxSize := int64(maxSizeMB) * 1024 * 1024
+    logger := &RotatingLogger{
+        basePath:    basePath,
+        maxSize:     maxSize,
+        compressOld: compress,
+        sequence:    0,
+    }
+
+    err := logger.openCurrentFile()
+    if err != nil {
+        return nil, err
+    }
+
+    return logger, nil
+}
+
+func (rl *RotatingLogger) openCurrentFile() error {
+    rl.mu.Lock()
+    defer rl.mu.Unlock()
+
+    if rl.currentFile != nil {
+        rl.currentFile.Close()
+    }
+
+    file, err := os.OpenFile(rl.basePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+    if err != nil {
+        return err
+    }
+
+    info, err := file.Stat()
+    if err != nil {
+        file.Close()
+        return err
+    }
+
+    rl.currentFile = file
+    rl.currentSize = info.Size()
+    rl.sequence = rl.findLatestSequence()
+
+    return nil
+}
+
+func (rl *RotatingLogger) findLatestSequence() int {
+    pattern := rl.basePath + ".*"
+    matches, _ := filepath.Glob(pattern)
+
+    maxSeq := 0
+    for _, match := range matches {
+        base := filepath.Base(match)
+        suffix := strings.TrimPrefix(base, filepath.Base(rl.basePath)+".")
+        if idx := strings.Index(suffix, "."); idx != -1 {
+            suffix = suffix[:idx]
+        }
+        if seq, err := strconv.Atoi(suffix); err == nil && seq > maxSeq {
+            maxSeq = seq
+        }
+    }
+    return maxSeq
+}
+
+func (rl *RotatingLogger) Write(p []byte) (int, error) {
+    rl.mu.Lock()
+    defer rl.mu.Unlock()
+
+    if rl.currentSize+int64(len(p)) > rl.maxSize {
+        err := rl.rotate()
+        if err != nil {
+            return 0, err
+        }
+    }
+
+    n, err := rl.currentFile.Write(p)
+    if err == nil {
+        rl.currentSize += int64(n)
+    }
+    return n, err
+}
+
+func (rl *RotatingLogger) rotate() error {
+    if rl.currentFile != nil {
+        rl.currentFile.Close()
+        rl.currentFile = nil
+    }
+
+    rl.sequence++
+    rotatedPath := fmt.Sprintf("%s.%d", rl.basePath, rl.sequence)
+
+    err := os.Rename(rl.basePath, rotatedPath)
+    if err != nil {
+        return err
+    }
+
+    if rl.compressOld {
+        go rl.compressFile(rotatedPath)
+    }
+
+    return rl.openCurrentFile()
+}
+
+func (rl *RotatingLogger) compressFile(path string) {
+    compressedPath := path + ".gz"
+
+    src, err := os.Open(path)
+    if err != nil {
+        return
+    }
+    defer src.Close()
+
+    dst, err := os.Create(compressedPath)
+    if err != nil {
+        return
+    }
+    defer dst.Close()
+
+    gz := gzip.NewWriter(dst)
+    defer gz.Close()
+
+    _, err = io.Copy(gz, src)
+    if err != nil {
+        os.Remove(compressedPath)
+        return
+    }
+
+    os.Remove(path)
+}
+
+func (rl *RotatingLogger) Close() error {
+    rl.mu.Lock()
+    defer rl.mu.Unlock()
+
+    if rl.currentFile != nil {
+        return rl.currentFile.Close()
+    }
+    return nil
+}
+
+func main() {
+    logger, err := NewRotatingLogger("app.log", 10, true)
+    if err != nil {
+        fmt.Printf("Failed to create logger: %v\n", err)
+        return
+    }
+    defer logger.Close()
+
+    for i := 0; i < 1000; i++ {
+        msg := fmt.Sprintf("[%s] Log entry %d: Application event processed\n",
+            time.Now().Format("2006-01-02 15:04:05"), i)
+        logger.Write([]byte(msg))
+        time.Sleep(10 * time.Millisecond)
+    }
+
+    fmt.Println("Log rotation test completed")
 }

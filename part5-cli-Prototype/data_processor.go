b50404a366d5c180ea61ce0c428ead663bf3e2f3
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-09 19:56:44 +0000

    Add data validation and transformation logic for user input

--- part5-cli-Prototype/data_processor.go
+++ part5-cli-Prototype/data_processor.go
@@ -113,4 +113,53 @@ func main() {
 	}
 
 	fmt.Printf("Processed user data: %+v\n", processedData)
+}package main
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+type UserData struct {
+	Email    string
+	Username string
+	Age      int
+}
+
+func ValidateEmail(email string) error {
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	matched, err := regexp.MatchString(pattern, email)
+	if err != nil {
+		return err
+	}
+	if !matched {
+		return errors.New("invalid email format")
+	}
+	return nil
+}
+
+func SanitizeUsername(username string) string {
+	return strings.TrimSpace(username)
+}
+
+func ValidateAge(age int) error {
+	if age < 0 || age > 150 {
+		return errors.New("age must be between 0 and 150")
+	}
+	return nil
+}
+
+func ProcessUserData(data UserData) (UserData, error) {
+	if err := ValidateEmail(data.Email); err != nil {
+		return UserData{}, err
+	}
+
+	data.Username = SanitizeUsername(data.Username)
+
+	if err := ValidateAge(data.Age); err != nil {
+		return UserData{}, err
+	}
+
+	return data, nil
 }

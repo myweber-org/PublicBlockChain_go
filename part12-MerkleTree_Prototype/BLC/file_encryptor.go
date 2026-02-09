f016932e506c10c839be1fefecd5c2afb46c7886
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-09 23:39:24 +0000

    Add file encryption utility with AES-256-GCM support

--- part12-MerkleTree_Prototype/BLC/file_encryptor.go
+++ part12-MerkleTree_Prototype/BLC/file_encryptor.go
@@ -122,3 +122,159 @@ func main() {
 
 	fmt.Printf("%s completed successfully\n", action)
 }
+package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/hex"
+    "errors"
+    "fmt"
+    "io"
+    "os"
+)
+
+func encryptData(plaintext []byte, key []byte) ([]byte, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return nil, err
+    }
+
+    nonce := make([]byte, gcm.NonceSize())
+    if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+        return nil, err
+    }
+
+    ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+    return ciphertext, nil
+}
+
+func decryptData(ciphertext []byte, key []byte) ([]byte, error) {
+    block, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return nil, err
+    }
+
+    nonceSize := gcm.NonceSize()
+    if len(ciphertext) < nonceSize {
+        return nil, errors.New("ciphertext too short")
+    }
+
+    nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+    plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+    if err != nil {
+        return nil, err
+    }
+
+    return plaintext, nil
+}
+
+func generateRandomKey() ([]byte, error) {
+    key := make([]byte, 32)
+    if _, err := rand.Read(key); err != nil {
+        return nil, err
+    }
+    return key, nil
+}
+
+func main() {
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: file_encryptor <encrypt|decrypt|genkey>")
+        os.Exit(1)
+    }
+
+    switch os.Args[1] {
+    case "genkey":
+        key, err := generateRandomKey()
+        if err != nil {
+            fmt.Printf("Error generating key: %v\n", err)
+            os.Exit(1)
+        }
+        fmt.Printf("Generated key: %s\n", hex.EncodeToString(key))
+
+    case "encrypt":
+        if len(os.Args) != 4 {
+            fmt.Println("Usage: file_encryptor encrypt <input_file> <key_hex>")
+            os.Exit(1)
+        }
+
+        key, err := hex.DecodeString(os.Args[3])
+        if err != nil {
+            fmt.Printf("Invalid key format: %v\n", err)
+            os.Exit(1)
+        }
+
+        data, err := os.ReadFile(os.Args[2])
+        if err != nil {
+            fmt.Printf("Error reading file: %v\n", err)
+            os.Exit(1)
+        }
+
+        encrypted, err := encryptData(data, key)
+        if err != nil {
+            fmt.Printf("Error encrypting: %v\n", err)
+            os.Exit(1)
+        }
+
+        err = os.WriteFile(os.Args[2]+".enc", encrypted, 0644)
+        if err != nil {
+            fmt.Printf("Error writing file: %v\n", err)
+            os.Exit(1)
+        }
+
+        fmt.Printf("File encrypted successfully: %s.enc\n", os.Args[2])
+
+    case "decrypt":
+        if len(os.Args) != 4 {
+            fmt.Println("Usage: file_encryptor decrypt <encrypted_file> <key_hex>")
+            os.Exit(1)
+        }
+
+        key, err := hex.DecodeString(os.Args[3])
+        if err != nil {
+            fmt.Printf("Invalid key format: %v\n", err)
+            os.Exit(1)
+        }
+
+        data, err := os.ReadFile(os.Args[2])
+        if err != nil {
+            fmt.Printf("Error reading file: %v\n", err)
+            os.Exit(1)
+        }
+
+        decrypted, err := decryptData(data, key)
+        if err != nil {
+            fmt.Printf("Error decrypting: %v\n", err)
+            os.Exit(1)
+        }
+
+        outputFile := os.Args[2]
+        if len(outputFile) > 4 && outputFile[len(outputFile)-4:] == ".enc" {
+            outputFile = outputFile[:len(outputFile)-4]
+        }
+        outputFile = outputFile + ".dec"
+
+        err = os.WriteFile(outputFile, decrypted, 0644)
+        if err != nil {
+            fmt.Printf("Error writing file: %v\n", err)
+            os.Exit(1)
+        }
+
+        fmt.Printf("File decrypted successfully: %s\n", outputFile)
+
+    default:
+        fmt.Println("Invalid command. Use: encrypt, decrypt, or genkey")
+        os.Exit(1)
+    }
+}

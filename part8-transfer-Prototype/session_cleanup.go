d9dcf62642176239010bc0ad434cec20f3313f44
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-09 19:39:01 +0000

    Add session cleanup job to delete expired sessions from the database

--- part8-transfer-Prototype/session_cleanup.go
+++ part8-transfer-Prototype/session_cleanup.go
@@ -49,4 +49,60 @@ func cleanupExpiredSessions(ctx context.Context, db *database.DB) error {
 
 	log.Printf("Cleaned up %d expired sessions", rowsAffected)
 	return nil
+}package main
+
+import (
+    "context"
+    "database/sql"
+    "log"
+    "time"
+)
+
+const cleanupInterval = 1 * time.Hour
+const sessionTTL = 24 * time.Hour
+
+type SessionCleaner struct {
+    db *sql.DB
+}
+
+func NewSessionCleaner(db *sql.DB) *SessionCleaner {
+    return &SessionCleaner{db: db}
+}
+
+func (sc *SessionCleaner) Run(ctx context.Context) {
+    ticker := time.NewTicker(cleanupInterval)
+    defer ticker.Stop()
+
+    for {
+        select {
+        case <-ctx.Done():
+            return
+        case <-ticker.C:
+            sc.cleanupExpiredSessions()
+        }
+    }
+}
+
+func (sc *SessionCleaner) cleanupExpiredSessions() {
+    ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+    defer cancel()
+
+    cutoffTime := time.Now().Add(-sessionTTL)
+
+    query := `DELETE FROM user_sessions WHERE last_activity < $1`
+    result, err := sc.db.ExecContext(ctx, query, cutoffTime)
+    if err != nil {
+        log.Printf("Failed to clean expired sessions: %v", err)
+        return
+    }
+
+    rowsAffected, err := result.RowsAffected()
+    if err != nil {
+        log.Printf("Failed to get rows affected: %v", err)
+        return
+    }
+
+    if rowsAffected > 0 {
+        log.Printf("Cleaned up %d expired sessions", rowsAffected)
+    }
 }

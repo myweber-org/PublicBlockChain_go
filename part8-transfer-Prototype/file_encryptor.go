802fbbc42f2b60cea6e214da3f2df14d3060c7f2
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-10 01:19:43 +0000

    Add file encryption utility with AES-256-CBC support

--- part8-transfer-Prototype/file_encryptor.go
+++ part8-transfer-Prototype/file_encryptor.go
@@ -323,4 +323,81 @@ func main() {
 		fmt.Println("Invalid action. Use 'encrypt' or 'decrypt'")
 		os.Exit(1)
 	}
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"os"
+)
+
+func encryptData(plaintext []byte, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func decryptData(ciphertext string, key []byte) ([]byte, error) {
+	data, err := base64.StdEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) < aes.BlockSize {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+
+	iv := data[:aes.BlockSize]
+	data = data[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(data, data)
+
+	return data, nil
+}
+
+func main() {
+	key := make([]byte, 32)
+	if _, err := rand.Read(key); err != nil {
+		fmt.Printf("Key generation error: %v\n", err)
+		os.Exit(1)
+	}
+
+	secretMessage := "Confidential data requiring protection"
+	encrypted, err := encryptData([]byte(secretMessage), key)
+	if err != nil {
+		fmt.Printf("Encryption error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Encrypted: %s\n", encrypted)
+
+	decrypted, err := decryptData(encrypted, key)
+	if err != nil {
+		fmt.Printf("Decryption error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Decrypted: %s\n", string(decrypted))
 }

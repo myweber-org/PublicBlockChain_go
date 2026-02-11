31a8a63e1f0a1ca05ae95ec66963f9d473f70756
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-02-11 19:46:49 +0000

    Add configuration loader with YAML parsing support

--- part8-transfer-Prototype/BLC/config_loader.go
+++ part8-transfer-Prototype/BLC/config_loader.go
@@ -53,4 +53,47 @@ func validateConfig(config *ServerConfig) error {
     }
 
     return nil
+}package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+    Server struct {
+        Host string `yaml:"host"`
+        Port int    `yaml:"port"`
+    } `yaml:"server"`
+    Database struct {
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+        Name     string `yaml:"name"`
+    } `yaml:"database"`
+    LogLevel string `yaml:"log_level"`
+}
+
+func LoadConfig(path string) (*AppConfig, error) {
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    if err := yaml.Unmarshal(data, &config); err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    return &config, nil
+}
+
+func (c *AppConfig) Validate() error {
+    if c.Server.Port <= 0 || c.Server.Port > 65535 {
+        return fmt.Errorf("invalid port number: %d", c.Server.Port)
+    }
+    if c.Database.Name == "" {
+        return fmt.Errorf("database name cannot be empty")
+    }
+    return nil
 }

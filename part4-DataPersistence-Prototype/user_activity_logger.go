9b86599c986619e1c6bee7457590505faf2a0e9a
InkspillVerse <inkspill.verse@outlook.com>
2026-02-12 16:39:02 +0000

    Add user activity logging middleware for API endpoints

--- part4-DataPersistence-Prototype/user_activity_logger.go
+++ part4-DataPersistence-Prototype/user_activity_logger.go
@@ -43,3 +43,37 @@ func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
 }
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	userID := extractUserID(r)
+	ipAddress := r.RemoteAddr
+
+	al.handler.ServeHTTP(w, r)
+
+	duration := time.Since(start)
+	log.Printf("User %s from IP %s accessed %s %s - Duration: %v",
+		userID, ipAddress, r.Method, r.URL.Path, duration)
+}
+
+func extractUserID(r *http.Request) string {
+	if authHeader := r.Header.Get("Authorization"); authHeader != "" {
+		return "authenticated_user"
+	}
+	return "anonymous_user"
+}

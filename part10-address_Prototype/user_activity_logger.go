2792e3613c125de909776b37f4f2e0ba58733bdf
heyQuim <hey.quim@hotmail.com>
2026-02-12 14:46:21 +0000

    Add user activity logging middleware with request details and timestamp

--- part10-address_Prototype/user_activity_logger.go
+++ part10-address_Prototype/user_activity_logger.go
@@ -25,4 +25,38 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.RemoteAddr,
 		duration,
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
+}
+
+func ActivityLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		recorder := &responseRecorder{ResponseWriter: w, statusCode: http.StatusOK}
+		
+		next.ServeHTTP(recorder, r)
+		
+		duration := time.Since(start)
+		log.Printf("[%s] %s %s %d %v",
+			time.Now().Format("2006-01-02 15:04:05"),
+			r.Method,
+			r.URL.Path,
+			recorder.statusCode,
+			duration,
+		)
+	})
 }

92a1de6d4e6b99d6b61e50a186aaa148e4f10398
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-13 18:41:19 +0000

    Add user activity logging middleware for HTTP requests

--- part13-Network_Prototype/BLC/user_activity_logger.go
+++ part13-Network_Prototype/BLC/user_activity_logger.go
@@ -16,23 +16,13 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	
 	recorder := &responseRecorder{
 		ResponseWriter: w,
 		statusCode:     http.StatusOK,
 	}
-	
 	al.handler.ServeHTTP(recorder, r)
-	
 	duration := time.Since(start)
-	
-	log.Printf("%s %s %d %s %s",
-		r.Method,
-		r.URL.Path,
-		recorder.statusCode,
-		duration.String(),
-		r.RemoteAddr,
-	)
+	log.Printf("%s %s %d %v", r.Method, r.URL.Path, recorder.statusCode, duration)
 }
 
 type responseRecorder struct {
@@ -43,87 +33,4 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
-}package main
-
-import (
-    "encoding/json"
-    "fmt"
-    "os"
-    "time"
-)
-
-type UserActivity struct {
-    UserID    string    `json:"user_id"`
-    Action    string    `json:"action"`
-    Timestamp time.Time `json:"timestamp"`
-    Details   string    `json:"details,omitempty"`
-}
-
-func logActivity(userID, action, details string) UserActivity {
-    activity := UserActivity{
-        UserID:    userID,
-        Action:    action,
-        Timestamp: time.Now().UTC(),
-        Details:   details,
-    }
-    
-    return activity
-}
-
-func saveActivityLog(activity UserActivity, filename string) error {
-    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-
-    data, err := json.Marshal(activity)
-    if err != nil {
-        return err
-    }
-
-    _, err = file.Write(append(data, '\n'))
-    return err
-}
-
-func main() {
-    activity := logActivity("user123", "login", "Successful authentication")
-    
-    err := saveActivityLog(activity, "user_activities.log")
-    if err != nil {
-        fmt.Printf("Error saving log: %v\n", err)
-        return
-    }
-    
-    fmt.Printf("Activity logged: %s performed %s at %s\n", 
-        activity.UserID, activity.Action, activity.Timestamp.Format(time.RFC3339))
 }

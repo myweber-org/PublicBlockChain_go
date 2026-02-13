577216133c8f8a0568a8cffbac9434260bb20944
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-02-13 19:54:14 +0000

    Add user activity logging middleware for tracking API requests

--- part3-boltdb-Prototype/BLC/user_activity_logger.go
+++ part3-boltdb-Prototype/BLC/user_activity_logger.go
@@ -16,78 +16,26 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	al.handler.ServeHTTP(w, r)
+	recorder := &responseRecorder{ResponseWriter: w, statusCode: http.StatusOK}
+	
+	al.handler.ServeHTTP(recorder, r)
+	
 	duration := time.Since(start)
-
-	log.Printf(
-		"User activity: %s %s from %s completed in %v",
+	log.Printf("%s %s %d %s %s",
 		r.Method,
 		r.URL.Path,
-		r.RemoteAddr,
+		recorder.statusCode,
 		duration,
+		r.RemoteAddr,
 	)
-}package middleware
+}
 
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type responseWriter struct {
+type responseRecorder struct {
 	http.ResponseWriter
 	statusCode int
 }
 
-func (rw *responseWriter) WriteHeader(code int) {
-	rw.statusCode = code
-	rw.ResponseWriter.WriteHeader(code)
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-
-		next.ServeHTTP(rw, r)
-
-		duration := time.Since(start)
-		log.Printf(
-			"[%s] %s %s %d %v",
-			time.Now().Format(time.RFC3339),
-			r.Method,
-			r.URL.Path,
-			rw.statusCode,
-			duration,
-		)
-	})
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	startTime := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(startTime)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }

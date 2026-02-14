6fee0744131458b34c936b9cac4795a53b8d7b98
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-14 00:33:17 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- part1-Basic-Prototype/BLC/data_cleaner.go
+++ part1-Basic-Prototype/BLC/data_cleaner.go
@@ -188,4 +188,26 @@ func main() {
     emails := []string{"a@b.com", "c@d.com", "a@b.com", "e@f.com"}
     unique := DeduplicateEmails(emails)
     fmt.Println("Unique emails:", unique)
+}package main
+
+import "fmt"
+
+func RemoveDuplicates(input []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, value := range input {
+		if !seen[value] {
+			seen[value] = true
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func main() {
+	numbers := []int{1, 2, 2, 3, 4, 4, 5, 6, 6, 7}
+	uniqueNumbers := RemoveDuplicates(numbers)
+	fmt.Println("Original:", numbers)
+	fmt.Println("Unique:", uniqueNumbers)
 }

01d6d12a78c099cd83a434a225a0fa42714573b9
heyQuim <hey.quim@hotmail.com>
2026-02-14 16:43:31 +0000

    Add user activity logging middleware with rate limiting and Redis support

--- part3-boltdb-Prototype/BLC/user_activity_logger.go
+++ part3-boltdb-Prototype/BLC/user_activity_logger.go
@@ -1,41 +1,77 @@
-package middleware
+package main
 
 import (
-	"log"
-	"net/http"
-	"time"
+    "context"
+    "fmt"
+    "net/http"
+    "time"
+
+    "github.com/go-redis/redis/v8"
+    "golang.org/x/time/rate"
 )
 
 type ActivityLogger struct {
-	handler http.Handler
+    redisClient *redis.Client
+    limiter     *rate.Limiter
 }
 
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
+func NewActivityLogger(redisAddr string, rps int) *ActivityLogger {
+    rdb := redis.NewClient(&redis.Options{
+        Addr:     redisAddr,
+        Password: "",
+        DB:       0,
+    })
+
+    return &ActivityLogger{
+        redisClient: rdb,
+        limiter:     rate.NewLimiter(rate.Limit(rps), rps*2),
+    }
 }
 
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	recorder := &responseRecorder{ResponseWriter: w, statusCode: http.StatusOK}
-	
-	al.handler.ServeHTTP(recorder, r)
-	
-	duration := time.Since(start)
-	log.Printf("%s %s %d %s %s",
-		r.Method,
-		r.URL.Path,
-		recorder.statusCode,
-		duration,
-		r.RemoteAddr,
-	)
+func (al *ActivityLogger) LogActivity(userID, action string) error {
+    if !al.limiter.Allow() {
+        return fmt.Errorf("rate limit exceeded")
+    }
+
+    ctx := context.Background()
+    key := fmt.Sprintf("activity:%s:%d", userID, time.Now().Unix())
+    
+    activity := map[string]interface{}{
+        "user_id":    userID,
+        "action":     action,
+        "timestamp":  time.Now().Format(time.RFC3339),
+        "user_agent": "web-client",
+    }
+
+    err := al.redisClient.HSet(ctx, key, activity).Err()
+    if err != nil {
+        return fmt.Errorf("failed to log activity: %w", err)
+    }
+
+    al.redisClient.Expire(ctx, key, 24*time.Hour)
+    return nil
 }
 
-type responseRecorder struct {
-	http.ResponseWriter
-	statusCode int
+func (al *ActivityLogger) Middleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        userID := r.Header.Get("X-User-ID")
+        if userID != "" {
+            go al.LogActivity(userID, r.URL.Path)
+        }
+        next.ServeHTTP(w, r)
+    })
 }
 
-func (rr *responseRecorder) WriteHeader(code int) {
-	rr.statusCode = code
-	rr.ResponseWriter.WriteHeader(code)
+func main() {
+    logger := NewActivityLogger("localhost:6379", 10)
+    
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Request processed")
+    })
+
+    wrappedMux := logger.Middleware(mux)
+    
+    fmt.Println("Server starting on :8080")
+    http.ListenAndServe(":8080", wrappedMux)
 }

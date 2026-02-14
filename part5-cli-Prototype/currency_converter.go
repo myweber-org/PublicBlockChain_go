90e627c1e4e2339714a2770a269b5b8baa3b58e9
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-14 21:39:20 +0000

    Add currency conversion utility with USD to EUR rate

--- part5-cli-Prototype/currency_converter.go
+++ part5-cli-Prototype/currency_converter.go
@@ -233,4 +233,20 @@ func main() {
 	converter.AddRate("CAD", 1.25)
 	cadResult, _ := converter.Convert(amount, USD, "CAD")
 	fmt.Printf("%.2f %s = %.2f CAD\n", amount, USD, cadResult)
+}package main
+
+import (
+	"fmt"
+)
+
+const usdToEurRate = 0.85
+
+func ConvertUSDToEUR(amount float64) float64 {
+	return amount * usdToEurRate
+}
+
+func main() {
+	usdAmount := 100.0
+	eurAmount := ConvertUSDToEUR(usdAmount)
+	fmt.Printf("%.2f USD = %.2f EUR\n", usdAmount, eurAmount)
 }

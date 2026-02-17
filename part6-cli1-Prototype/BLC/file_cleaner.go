8f105221f3742ed1f6ef4496eff94030a8992a0d
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-02-17 16:30:15 +0000

    Add utility to remove temporary files older than 7 days

--- part6-cli1-Prototype/BLC/file_cleaner.go
+++ part6-cli1-Prototype/BLC/file_cleaner.go
@@ -1,25 +1,48 @@
 package main
 
 import (
-    "os"
-    "path/filepath"
-    "time"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	tempDir      = "/tmp/app_cache"
+	retentionDays = 7
 )
 
 func main() {
-    dir := "/tmp"
-    cutoff := time.Now().AddDate(0, 0, -7)
+	err := cleanOldFiles(tempDir, retentionDays)
+	if err != nil {
+		fmt.Printf("Cleanup failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
 
-    filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return nil
-        }
-        if info.IsDir() {
-            return nil
-        }
-        if info.ModTime().Before(cutoff) {
-            os.Remove(path)
-        }
-        return nil
-    })
+func cleanOldFiles(dirPath string, days int) error {
+	cutoffTime := time.Now().AddDate(0, 0, -days)
+
+	return filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		info, err := d.Info()
+		if err != nil {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			fmt.Printf("Removing: %s (modified: %v)\n", path, info.ModTime())
+			return os.Remove(path)
+		}
+		return nil
+	})
 }

cf3f4ed88d4e15d8e1c3d40810ad64138644cfab
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-17 19:55:53 +0000

    Add utility to remove temporary files older than 7 days

--- part6-cli1-Prototype/file_cleaner.go
+++ part6-cli1-Prototype/file_cleaner.go
@@ -8,14 +8,11 @@ import (
 	"time"
 )
 
-const (
-	tempDir      = "/tmp/app_temp"
-	daysToKeep   = 7
-	fileExt      = ".tmp"
-)
+const tempDir = "/tmp/myapp"
+const retentionDays = 7
 
 func main() {
-	err := cleanOldFiles(tempDir, daysToKeep, fileExt)
+	err := cleanOldFiles(tempDir, retentionDays)
 	if err != nil {
 		fmt.Printf("Error cleaning files: %v\n", err)
 		os.Exit(1)
@@ -23,32 +20,27 @@ func main() {
 	fmt.Println("Cleanup completed successfully")
 }
 
-func cleanOldFiles(dirPath string, daysOld int, extension string) error {
+func cleanOldFiles(dirPath string, days int) error {
 	files, err := ioutil.ReadDir(dirPath)
 	if err != nil {
-		return fmt.Errorf("failed to read directory: %w", err)
+		return err
 	}
 
-	cutoffTime := time.Now().AddDate(0, 0, -daysOld)
+	cutoffTime := time.Now().AddDate(0, 0, -days)
 	removedCount := 0
 
 	for _, file := range files {
-		if filepath.Ext(file.Name()) != extension {
-			continue
-		}
-
 		if file.ModTime().Before(cutoffTime) {
 			fullPath := filepath.Join(dirPath, file.Name())
 			err := os.Remove(fullPath)
 			if err != nil {
-				fmt.Printf("Warning: Could not remove %s: %v\n", fullPath, err)
-				continue
+				fmt.Printf("Failed to remove %s: %v\n", fullPath, err)
+			} else {
+				removedCount++
 			}
-			removedCount++
-			fmt.Printf("Removed: %s\n", fullPath)
 		}
 	}
 
-	fmt.Printf("Total files removed: %d\n", removedCount)
+	fmt.Printf("Removed %d files older than %d days\n", removedCount, days)
 	return nil
 }

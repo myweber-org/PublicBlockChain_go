77011ee7fbfb8a8a4425f8cbc00c526325ea8512
heyQuim <hey.quim@hotmail.com>
2026-02-19 23:45:58 +0000

    Add data processing utility for cleaning and normalizing input strings

--- part11-transaction_1_Prototype/data_processor.go
+++ part11-transaction_1_Prototype/data_processor.go
@@ -253,3 +253,40 @@ func ProcessUserInput(rawUsername string, rawEmail string, rawAge int) (UserData
 	userData = TransformUsername(userData)
 	return userData, nil
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	cleaned := dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+	return cleaned
+}
+
+func (dp *DataProcessor) NormalizeCase(input string) string {
+	return strings.ToLower(dp.CleanString(input))
+}
+
+func (dp *DataProcessor) RemoveSpecialChars(input string) string {
+	reg := regexp.MustCompile(`[^a-zA-Z0-9\s]`)
+	return reg.ReplaceAllString(input, "")
+}
+
+func (dp *DataProcessor) Process(input string) string {
+	cleaned := dp.CleanString(input)
+	normalized := dp.NormalizeCase(cleaned)
+	return dp.RemoveSpecialChars(normalized)
+}

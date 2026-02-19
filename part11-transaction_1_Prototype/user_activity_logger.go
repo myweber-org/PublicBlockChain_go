1bf3347c93853b2eed2ea0023139e5e28fc6d41e
FelixMakes <felix.makes@hotmail.com>
2026-02-19 00:46:48 +0000

    Add user activity logging middleware for tracking API usage

--- part11-transaction_1_Prototype/user_activity_logger.go
+++ part11-transaction_1_Prototype/user_activity_logger.go
@@ -143,4 +143,50 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		duration,
 		time.Now().Format(time.RFC3339),
 	)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+	
+	responseRecorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+	
+	al.handler.ServeHTTP(responseRecorder, r)
+	
+	duration := time.Since(startTime)
+	
+	log.Printf(
+		"Activity: %s %s | Status: %d | Duration: %v | User-Agent: %s",
+		r.Method,
+		r.URL.Path,
+		responseRecorder.statusCode,
+		duration,
+		r.UserAgent(),
+	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(statusCode int) {
+	rr.statusCode = statusCode
+	rr.ResponseWriter.WriteHeader(statusCode)
 }

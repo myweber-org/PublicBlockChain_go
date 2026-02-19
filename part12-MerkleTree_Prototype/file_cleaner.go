39525897ff4e0e6d4ffd671b059ff61814fc4fc6
ElaraWrites <elara.writes@outlook.com>
2026-02-19 14:50:31 +0000

    Add utility to remove temporary files older than 7 days

---
+++ part12-MerkleTree_Prototype/file_cleaner.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	tempDir      = "/tmp/app_cache"
+	retentionDays = 7
+)
+
+func main() {
+	err := cleanOldFiles(tempDir)
+	if err != nil {
+		fmt.Printf("Cleanup failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles(dirPath string) error {
+	cutoffTime := time.Now().AddDate(0, 0, -retentionDays)
+
+	return filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		info, err := d.Info()
+		if err != nil {
+			return err
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			fmt.Printf("Removing: %s (modified: %v)\n", path, info.ModTime())
+			return os.Remove(path)
+		}
+		return nil
+	})
+}

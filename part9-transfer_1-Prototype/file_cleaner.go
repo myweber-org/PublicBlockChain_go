eb4740d69c1743152671c3c5da7fcf52115e9242
InkspillVerse <inkspill.verse@outlook.com>
2026-02-20 20:45:03 +0000

    Add utility to remove temporary files older than 7 days

--- part9-transfer_1-Prototype/file_cleaner.go
+++ part9-transfer_1-Prototype/file_cleaner.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-    "os"
-    "path/filepath"
-    "time"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 )
 
+const retentionDays = 7
+
 func main() {
-    tempDir := os.TempDir()
-    cutoff := time.Now().AddDate(0, 0, -7)
+	tempDir := os.TempDir()
+	cutoffTime := time.Now().AddDate(0, 0, -retentionDays)
 
-    filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return nil
-        }
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
 
-        if !info.IsDir() && info.ModTime().Before(cutoff) {
-            os.Remove(path)
-        }
-        return nil
-    })
+		if info.IsDir() {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			fmt.Printf("Removing old file: %s\n", path)
+			os.Remove(path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error cleaning temp directory: %v\n", err)
+	}
 }

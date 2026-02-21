88406577e46d377d21b4de81a1c8ec1143d79bd4
FelixMakes <felix.makes@hotmail.com>
2026-02-21 15:34:18 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- part1-Basic-Prototype/data_cleaner.go
+++ part1-Basic-Prototype/data_cleaner.go
@@ -353,3 +353,71 @@ func main() {
 		fmt.Printf("ID: %d, Name: %s, Email: %s\n", record.ID, record.Name, record.Email)
 	}
 }
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func removeDuplicates(inputFile, outputFile string) error {
+	inFile, err := os.Open(inputFile)
+	if err != nil {
+		return fmt.Errorf("failed to open input file: %w", err)
+	}
+	defer inFile.Close()
+
+	reader := csv.NewReader(inFile)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return fmt.Errorf("failed to read CSV: %w", err)
+	}
+
+	seen := make(map[string]bool)
+	var uniqueRecords [][]string
+
+	for _, record := range records {
+		if len(record) == 0 {
+			continue
+		}
+		key := record[0]
+		if !seen[key] {
+			seen[key] = true
+			uniqueRecords = append(uniqueRecords, record)
+		}
+	}
+
+	outFile, err := os.Create(outputFile)
+	if err != nil {
+		return fmt.Errorf("failed to create output file: %w", err)
+	}
+	defer outFile.Close()
+
+	writer := csv.NewWriter(outFile)
+	err = writer.WriteAll(uniqueRecords)
+	if err != nil {
+		return fmt.Errorf("failed to write CSV: %w", err)
+	}
+	writer.Flush()
+
+	return nil
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	err := removeDuplicates(inputFile, outputFile)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully cleaned data. Output saved to %s\n", outputFile)
+}

36651831445c8958ca4d7cdf36556e2f2e66af8d
VioletInquiry <violet.inquiry@outlook.com>
2026-02-21 22:39:24 +0000

    Add utility to remove duplicate lines from a text file

--- part6-cli1-Prototype/file_cleaner.go
+++ part6-cli1-Prototype/file_cleaner.go
@@ -43,4 +43,83 @@ func cleanOldFiles(dirPath string, days int) error {
 
 	fmt.Printf("Removed %d files older than %d days\n", removedCount, days)
 	return nil
+}package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: file_cleaner <input_file> [output_file]")
+		return
+	}
+
+	inputFile := os.Args[1]
+	outputFile := "cleaned_" + inputFile
+	if len(os.Args) > 2 {
+		outputFile = os.Args[2]
+	}
+
+	lines, err := readLines(inputFile)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	uniqueLines := removeDuplicates(lines)
+
+	err = writeLines(outputFile, uniqueLines)
+	if err != nil {
+		fmt.Printf("Error writing file: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Successfully cleaned file. Unique lines: %d -> %d\n", len(lines), len(uniqueLines))
+}
+
+func readLines(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func removeDuplicates(lines []string) []string {
+	seen := make(map[string]bool)
+	var result []string
+	for _, line := range lines {
+		if !seen[line] {
+			seen[line] = true
+			result = append(result, line)
+		}
+	}
+	return result
+}
+
+func writeLines(filename string, lines []string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for _, line := range lines {
+		_, err := writer.WriteString(line + "\n")
+		if err != nil {
+			return err
+		}
+	}
+	return writer.Flush()
 }

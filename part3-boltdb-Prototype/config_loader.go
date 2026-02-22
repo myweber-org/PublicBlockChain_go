e20f648fca4d67b47c36b1460894b2e6da0dc75e
YourApexArchive <your.apex.archive@hotmail.com>
2026-02-22 18:12:31 +0000

    Add configuration loader with environment variable support and validation

--- part3-boltdb-Prototype/config_loader.go
+++ part3-boltdb-Prototype/config_loader.go
@@ -154,3 +154,55 @@ func getEnvAsSlice(key string, defaultValue []string, sep string) []string {
     }
     return strings.Split(valueStr, sep)
 }
+package config
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	ServerPort int
+	DBHost     string
+	DBPort     int
+	DebugMode  bool
+	AllowedIPs []string
+}
+
+func LoadConfig() (*Config, error) {
+	cfg := &Config{}
+
+	port, err := strconv.Atoi(getEnv("SERVER_PORT", "8080"))
+	if err != nil {
+		return nil, err
+	}
+	cfg.ServerPort = port
+
+	cfg.DBHost = getEnv("DB_HOST", "localhost")
+
+	dbPort, err := strconv.Atoi(getEnv("DB_PORT", "5432"))
+	if err != nil {
+		return nil, err
+	}
+	cfg.DBPort = dbPort
+
+	debug, err := strconv.ParseBool(getEnv("DEBUG_MODE", "false"))
+	if err != nil {
+		return nil, err
+	}
+	cfg.DebugMode = debug
+
+	ips := getEnv("ALLOWED_IPS", "127.0.0.1,::1")
+	cfg.AllowedIPs = strings.Split(ips, ",")
+
+	return cfg, nil
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}

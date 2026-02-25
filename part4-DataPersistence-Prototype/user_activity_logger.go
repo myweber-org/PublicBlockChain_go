3a831df24116aa806d18c25c6aa6aecd18983003
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-25 20:35:30 +0000

    Add user activity logging middleware for tracking API usage

--- part4-DataPersistence-Prototype/user_activity_logger.go
+++ part4-DataPersistence-Prototype/user_activity_logger.go
@@ -16,58 +16,30 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
 
+	al.handler.ServeHTTP(recorder, r)
+
+	duration := time.Since(start)
 	log.Printf(
-		"Method: %s | Path: %s | Duration: %v | Timestamp: %s",
+		"Method: %s | Path: %s | Status: %d | Duration: %v | RemoteAddr: %s",
 		r.Method,
 		r.URL.Path,
+		recorder.statusCode,
 		duration,
-		time.Now().Format(time.RFC3339),
+		r.RemoteAddr,
 	)
-}package main
-
-import (
-    "encoding/json"
-    "log"
-    "os"
-    "time"
-)
-
-type UserActivity struct {
-    Timestamp time.Time `json:"timestamp"`
-    UserID    string    `json:"user_id"`
-    Action    string    `json:"action"`
-    Details   string    `json:"details,omitempty"`
 }
 
-func LogActivity(userID, action, details string) error {
-    activity := UserActivity{
-        Timestamp: time.Now().UTC(),
-        UserID:    userID,
-        Action:    action,
-        Details:   details,
-    }
-
-    file, err := os.OpenFile("activity.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
-
-    encoder := json.NewEncoder(file)
-    encoder.SetIndent("", "  ")
-    if err := encoder.Encode(activity); err != nil {
-        return err
-    }
-
-    return nil
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
 }
 
-func main() {
-    if err := LogActivity("user123", "login", "Successful authentication"); err != nil {
-        log.Fatal(err)
-    }
-    log.Println("Activity logged successfully")
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }

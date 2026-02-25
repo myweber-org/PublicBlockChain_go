259d69334a6eb1d60325c201ff9de20fc0e63100
ElaraWrites <elara.writes@outlook.com>
2026-02-25 16:08:38 +0000

    Add configuration loader with YAML support and environment variable overrides

--- part3-boltdb-Prototype/config_loader.go
+++ part3-boltdb-Prototype/config_loader.go
@@ -280,4 +280,72 @@ func ValidateConfig(config *AppConfig) error {
     }
 
     return nil
+}package config
+
+import (
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Username string `yaml:"username" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASS"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+	} `yaml:"database"`
+	LogLevel string `yaml:"log_level" env:"LOG_LEVEL"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	config := &Config{}
+	if err := yaml.Unmarshal(data, config); err != nil {
+		return nil, err
+	}
+
+	overrideFromEnv(config)
+	return config, nil
+}
+
+func overrideFromEnv(c *Config) {
+	if val := os.Getenv("SERVER_HOST"); val != "" {
+		c.Server.Host = val
+	}
+	if val := os.Getenv("SERVER_PORT"); val != "" {
+		if port, err := strconv.Atoi(val); err == nil {
+			c.Server.Port = port
+		}
+	}
+	if val := os.Getenv("DB_HOST"); val != "" {
+		c.Database.Host = val
+	}
+	if val := os.Getenv("DB_PORT"); val != "" {
+		if port, err := strconv.Atoi(val); err == nil {
+			c.Database.Port = port
+		}
+	}
+	if val := os.Getenv("DB_USER"); val != "" {
+		c.Database.Username = val
+	}
+	if val := os.Getenv("DB_PASS"); val != "" {
+		c.Database.Password = val
+	}
+	if val := os.Getenv("DB_NAME"); val != "" {
+		c.Database.Name = val
+	}
+	if val := os.Getenv("LOG_LEVEL"); val != "" {
+		c.LogLevel = val
+	}
 }

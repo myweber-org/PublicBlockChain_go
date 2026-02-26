25f1888d4d963327c60383d558766faa1047634d
FelixMakes <felix.makes@hotmail.com>
2026-02-26 23:03:14 +0000

    Add AES-256 file encryption utility with secure key derivation

--- part5-cli-Prototype/BLC/file_encryption.go
+++ part5-cli-Prototype/BLC/file_encryption.go
@@ -86,4 +86,133 @@ func main() {
 		return
 	}
 	fmt.Printf("Decrypted: %s\n", decrypted)
+}package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+const (
+	saltSize   = 16
+	nonceSize  = 12
+	keyIter    = 100000
+)
+
+func deriveKey(password, salt []byte) []byte {
+	hash := sha256.New()
+	hash.Write(password)
+	hash.Write(salt)
+	for i := 0; i < keyIter; i++ {
+		hash.Write(hash.Sum(nil))
+	}
+	return hash.Sum(nil)
+}
+
+func encryptFile(inputPath, outputPath, password string) error {
+	salt := make([]byte, saltSize)
+	if _, err := rand.Read(salt); err != nil {
+		return err
+	}
+
+	key := deriveKey([]byte(password), salt)
+
+	plaintext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	nonce := make([]byte, nonceSize)
+	if _, err := rand.Read(nonce); err != nil {
+		return err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+
+	outputData := append(salt, nonce...)
+	outputData = append(outputData, ciphertext...)
+
+	return os.WriteFile(outputPath, outputData, 0644)
+}
+
+func decryptFile(inputPath, outputPath, password string) error {
+	ciphertext, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	if len(ciphertext) < saltSize+nonceSize {
+		return errors.New("file too short")
+	}
+
+	salt := ciphertext[:saltSize]
+	nonce := ciphertext[saltSize : saltSize+nonceSize]
+	actualCiphertext := ciphertext[saltSize+nonceSize:]
+
+	key := deriveKey([]byte(password), salt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	plaintext, err := aesgcm.Open(nil, nonce, actualCiphertext, nil)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputPath, plaintext, 0644)
+}
+
+func main() {
+	if len(os.Args) != 5 {
+		fmt.Printf("Usage: %s <encrypt|decrypt> <input> <output> <password>\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	operation := os.Args[1]
+	inputPath := os.Args[2]
+	outputPath := os.Args[3]
+	password := os.Args[4]
+
+	var err error
+	switch operation {
+	case "encrypt":
+		err = encryptFile(inputPath, outputPath, password)
+	case "decrypt":
+		err = decryptFile(inputPath, outputPath, password)
+	default:
+		fmt.Println("Invalid operation. Use 'encrypt' or 'decrypt'")
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Operation completed successfully")
 }

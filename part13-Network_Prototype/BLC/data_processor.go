bb794ad32db9c872f2a454a707c3361ee1845a47
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-27 20:56:44 +0000

    Add data validation and transformation functions for user input processing

--- part13-Network_Prototype/BLC/data_processor.go
+++ part13-Network_Prototype/BLC/data_processor.go
@@ -44,3 +44,61 @@ func ProcessUserInput(email, username string, age int) (UserData, error) {
 	err := ValidateUserData(userData)
 	return userData, err
 }
+package main
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+type UserData struct {
+	Username string
+	Email    string
+	Age      int
+}
+
+func ValidateUsername(username string) error {
+	if len(username) < 3 || len(username) > 20 {
+		return errors.New("username must be between 3 and 20 characters")
+	}
+	matched, _ := regexp.MatchString("^[a-zA-Z0-9_]+$", username)
+	if !matched {
+		return errors.New("username can only contain letters, numbers and underscores")
+	}
+	return nil
+}
+
+func ValidateEmail(email string) error {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !emailRegex.MatchString(email) {
+		return errors.New("invalid email format")
+	}
+	return nil
+}
+
+func NormalizeEmail(email string) string {
+	return strings.ToLower(strings.TrimSpace(email))
+}
+
+func ProcessUserData(data UserData) (UserData, error) {
+	if err := ValidateUsername(data.Username); err != nil {
+		return UserData{}, err
+	}
+	
+	if err := ValidateEmail(data.Email); err != nil {
+		return UserData{}, err
+	}
+	
+	data.Email = NormalizeEmail(data.Email)
+	
+	if data.Age < 0 || data.Age > 120 {
+		return UserData{}, errors.New("age must be between 0 and 120")
+	}
+	
+	return data, nil
+}
+
+func TransformUsername(username string) string {
+	return strings.ReplaceAll(username, "_", "-")
+}

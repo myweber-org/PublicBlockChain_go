b5afd7f4fe251001ac62fcd21d3ca012a3407b8f
InkspillVerse <inkspill.verse@outlook.com>
2026-02-27 14:44:33 +0000

    Add user activity logging middleware for tracking API usage

--- part4-DataPersistence-Prototype/BLC/user_activity_logger.go
+++ part4-DataPersistence-Prototype/BLC/user_activity_logger.go
@@ -16,14 +16,16 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
+	
 	recorder := &responseRecorder{
 		ResponseWriter: w,
 		statusCode:     http.StatusOK,
 	}
-
+	
 	al.handler.ServeHTTP(recorder, r)
-
+	
 	duration := time.Since(start)
+	
 	log.Printf(
 		"%s %s %d %s %s",
 		r.Method,
@@ -42,33 +44,4 @@ type responseRecorder struct {
 func (rr *responseRecorder) WriteHeader(code int) {
 	rr.statusCode = code
 	rr.ResponseWriter.WriteHeader(code)
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	
-	al.handler.ServeHTTP(w, r)
-	
-	duration := time.Since(start)
-	
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
-		r.RemoteAddr,
-		duration,
-	)
 }

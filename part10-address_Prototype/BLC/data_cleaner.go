290ccec83b3570522d4a3a2c6aed1989a8a9b05e
heyQuim <hey.quim@hotmail.com>
2026-02-28 14:35:30 +0000

    Add data cleaning utility to remove duplicate entries from a CSV file

--- part10-address_Prototype/BLC/data_cleaner.go
+++ part10-address_Prototype/BLC/data_cleaner.go
@@ -47,4 +47,71 @@ func main() {
 	cleaned := RemoveDuplicates(data)
 	fmt.Println("Original:", data)
 	fmt.Println("Cleaned:", cleaned)
+}package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+func removeDuplicates(inputPath, outputPath string) error {
+	inFile, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	reader := csv.NewReader(inFile)
+	writer := csv.NewWriter(outFile)
+	defer writer.Flush()
+
+	seen := make(map[string]bool)
+	headers, err := reader.Read()
+	if err != nil {
+		return err
+	}
+	if err := writer.Write(headers); err != nil {
+		return err
+	}
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		key := fmt.Sprintf("%v", record)
+		if !seen[key] {
+			seen[key] = true
+			if err := writer.Write(record); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: data_cleaner <input.csv> <output.csv>")
+		os.Exit(1)
+	}
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	if err := removeDuplicates(inputFile, outputFile); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Duplicate removal completed successfully")
 }

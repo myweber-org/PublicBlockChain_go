82e443f57211980915485004a23659f410bbc22d
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-28 14:51:40 +0000

    Add user activity logging middleware for tracking API usage

--- part1-Basic-Prototype/user_activity_logger.go
+++ part1-Basic-Prototype/user_activity_logger.go
@@ -16,75 +16,32 @@ func NewActivityLogger(handler http.Handler) *ActivityLogger {
 
 func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
-	al.handler.ServeHTTP(w, r)
+	
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+	
+	al.handler.ServeHTTP(recorder, r)
+	
 	duration := time.Since(start)
-
-	log.Printf("Activity: %s %s from %s took %v",
+	
+	log.Printf(
+		"%s %s %d %s %s",
 		r.Method,
 		r.URL.Path,
-		r.RemoteAddr,
+		recorder.statusCode,
 		duration,
-	)
-}package middleware
-
-import (
-    "log"
-    "net/http"
-    "time"
-)
-
-type responseWriter struct {
-    http.ResponseWriter
-    statusCode int
-}
-
-func (rw *responseWriter) WriteHeader(code int) {
-    rw.statusCode = code
-    rw.ResponseWriter.WriteHeader(code)
-}
-
-func ActivityLogger(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        start := time.Now()
-        rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
-
-        next.ServeHTTP(rw, r)
-
-        duration := time.Since(start)
-        log.Printf(
-            "%s %s %d %s %s",
-            r.Method,
-            r.URL.Path,
-            rw.statusCode,
-            duration,
-            r.RemoteAddr,
-        )
-    })
-}package middleware
-
-import (
-	"log"
-	"net/http"
-	"time"
-)
-
-type ActivityLogger struct {
-	handler http.Handler
-}
-
-func NewActivityLogger(handler http.Handler) *ActivityLogger {
-	return &ActivityLogger{handler: handler}
-}
-
-func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-	al.handler.ServeHTTP(w, r)
-	duration := time.Since(start)
-
-	log.Printf("Activity: %s %s from %s took %v",
-		r.Method,
-		r.URL.Path,
 		r.RemoteAddr,
-		duration,
 	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
 }

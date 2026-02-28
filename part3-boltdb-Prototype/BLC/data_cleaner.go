9dfcc8fb7d4de10d6c31d387fbff37a31b2db45e
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-02-28 20:24:54 +0000

    Add data cleaning utility to remove duplicate entries from a slice of integers

--- part3-boltdb-Prototype/BLC/data_cleaner.go
+++ part3-boltdb-Prototype/BLC/data_cleaner.go
@@ -93,3 +93,26 @@ func main() {
 	normalized := NormalizeWhitespace(text)
 	fmt.Println("Normalized:", normalized)
 }
+package main
+
+import "fmt"
+
+func RemoveDuplicates(nums []int) []int {
+	seen := make(map[int]bool)
+	result := []int{}
+
+	for _, num := range nums {
+		if !seen[num] {
+			seen[num] = true
+			result = append(result, num)
+		}
+	}
+	return result
+}
+
+func main() {
+	data := []int{1, 2, 2, 3, 4, 4, 5, 1, 6}
+	cleaned := RemoveDuplicates(data)
+	fmt.Println("Original:", data)
+	fmt.Println("Cleaned:", cleaned)
+}

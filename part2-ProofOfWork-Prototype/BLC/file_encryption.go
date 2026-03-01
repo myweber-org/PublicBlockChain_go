46dadfffd64f5bd86bf454f60ebb32ef0c250bbc
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2026-03-01 16:37:15 +0000

    Add file encryption utility with AES-256-GCM

--- part2-ProofOfWork-Prototype/BLC/file_encryption.go
+++ part2-ProofOfWork-Prototype/BLC/file_encryption.go
@@ -1,55 +1,76 @@
 package main
 
 import (
-    "crypto/aes"
-    "crypto/cipher"
-    "crypto/rand"
-    "encoding/base64"
-    "errors"
-    "io"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
 )
 
-func encryptData(plaintext []byte, key []byte) (string, error) {
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        return "", err
-    }
+func encrypt(plaintext []byte, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
 
-    gcm, err := cipher.NewGCM(block)
-    if err != nil {
-        return "", err
-    }
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
 
-    nonce := make([]byte, gcm.NonceSize())
-    if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-        return "", err
-    }
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
 
-    ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
-    return base64.StdEncoding.EncodeToString(ciphertext), nil
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
 
-func decryptData(encrypted string, key []byte) ([]byte, error) {
-    data, err := base64.StdEncoding.DecodeString(encrypted)
-    if err != nil {
-        return nil, err
-    }
+func decrypt(encodedCiphertext string, key []byte) ([]byte, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(encodedCiphertext)
+	if err != nil {
+		return nil, err
+	}
 
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        return nil, err
-    }
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
 
-    gcm, err := cipher.NewGCM(block)
-    if err != nil {
-        return nil, err
-    }
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
 
-    nonceSize := gcm.NonceSize()
-    if len(data) < nonceSize {
-        return nil, errors.New("ciphertext too short")
-    }
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
 
-    nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-    return gcm.Open(nil, nonce, ciphertext, nil)
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	return gcm.Open(nil, nonce, ciphertext, nil)
+}
+
+func main() {
+	key := make([]byte, 32)
+	if _, err := rand.Read(key); err != nil {
+		panic(err)
+	}
+
+	message := []byte("Sensitive data requiring protection")
+	
+	encrypted, err := encrypt(message, key)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Encrypted: %s\n", encrypted)
+
+	decrypted, err := decrypt(encrypted, key)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Decrypted: %s\n", decrypted)
 }

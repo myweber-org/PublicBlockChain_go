bd081c279868bc0bf55846873a813435d37d73e2
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-03-01 19:01:19 +0000

    Add utility to clean up temporary files in a directory

---
+++ part7-transaction-Prototype/file_cleaner.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	dir := "./tmp"
+	maxAge := time.Hour * 24 * 7
+
+	err := cleanOldFiles(dir, maxAge)
+	if err != nil {
+		fmt.Printf("Error cleaning files: %v\n", err)
+		return
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles(dir string, maxAge time.Duration) error {
+	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if time.Since(info.ModTime()) > maxAge {
+			fmt.Printf("Removing old file: %s\n", path)
+			return os.Remove(path)
+		}
+
+		return nil
+	})
+}

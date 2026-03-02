48cc38ed3c4ee86781c12fe44eee194107f71bd1
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-02 17:09:22 +0000

    Add configuration loader with YAML support and environment variable overrides

--- part6-cli1-Prototype/BLC/config_loader.go
+++ part6-cli1-Prototype/BLC/config_loader.go
@@ -239,4 +239,80 @@ func overrideFromEnv(c *Config) {
 	if val := os.Getenv("LOG_OUTPUT"); val != "" {
 		c.Logging.Output = val
 	}
+}package config
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		User     string `yaml:"user" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASSWORD"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Output string `yaml:"output" env:"LOG_OUTPUT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	if configPath == "" {
+		configPath = "config.yaml"
+	}
+
+	absPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	if err := cfg.loadFromEnv(); err != nil {
+		return nil, err
+	}
+
+	if err := cfg.validate(); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+func (c *Config) loadFromEnv() error {
+	// Environment variable loading would be implemented here
+	// using reflection to check env tags
+	return nil
+}
+
+func (c *Config) validate() error {
+	if c.Server.Host == "" {
+		return errors.New("server host cannot be empty")
+	}
+	if c.Server.Port <= 0 || c.Server.Port > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	if c.Database.Name == "" {
+		return errors.New("database name cannot be empty")
+	}
+	return nil
 }

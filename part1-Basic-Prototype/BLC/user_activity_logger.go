221f87945e2a8af82ae0ebdd4a2e6fd926573f1d
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-02 14:44:13 +0000

    Add user activity logging with timestamp and action type

--- part1-Basic-Prototype/BLC/user_activity_logger.go
+++ part1-Basic-Prototype/BLC/user_activity_logger.go
@@ -77,4 +77,79 @@ func ActivityLogger(next http.Handler) http.Handler {
 			r.RemoteAddr,
 		)
 	})
+}package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type Activity struct {
+	UserID    string    `json:"user_id"`
+	Action    string    `json:"action"`
+	Timestamp time.Time `json:"timestamp"`
+	Metadata  string    `json:"metadata,omitempty"`
+}
+
+type ActivityLogger struct {
+	logFile *os.File
+}
+
+func NewActivityLogger(filename string) (*ActivityLogger, error) {
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &ActivityLogger{logFile: file}, nil
+}
+
+func (l *ActivityLogger) LogActivity(userID, action, metadata string) error {
+	activity := Activity{
+		UserID:    userID,
+		Action:    action,
+		Timestamp: time.Now().UTC(),
+		Metadata:  metadata,
+	}
+
+	data, err := json.Marshal(activity)
+	if err != nil {
+		return err
+	}
+
+	data = append(data, '\n')
+	_, err = l.logFile.Write(data)
+	return err
+}
+
+func (l *ActivityLogger) Close() error {
+	return l.logFile.Close()
+}
+
+func main() {
+	logger, err := NewActivityLogger("activity.log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Close()
+
+	activities := []struct {
+		userID   string
+		action   string
+		metadata string
+	}{
+		{"user123", "login", "browser:chrome"},
+		{"user456", "upload", "file_size:2.5MB"},
+		{"user123", "logout", "session_duration:15m"},
+	}
+
+	for _, a := range activities {
+		if err := logger.LogActivity(a.userID, a.action, a.metadata); err != nil {
+			log.Printf("Failed to log activity: %v", err)
+		}
+	}
+
+	fmt.Println("Activity logging completed")
 }

83d5e8a2783420db96b5d02348a66cacf0ea51a0
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-02 15:38:10 +0000

    Add utility to remove temporary files older than 7 days

--- part9-transfer_1-Prototype/file_cleaner.go
+++ part9-transfer_1-Prototype/file_cleaner.go
@@ -60,4 +60,28 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error cleaning temp files: %v\n", err)
 	}
+}package main
+
+import (
+    "os"
+    "path/filepath"
+    "time"
+)
+
+func main() {
+    dir := "/tmp"
+    cutoff := time.Now().AddDate(0, 0, -7)
+
+    filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return nil
+        }
+        if info.IsDir() {
+            return nil
+        }
+        if info.ModTime().Before(cutoff) {
+            os.Remove(path)
+        }
+        return nil
+    })
 }

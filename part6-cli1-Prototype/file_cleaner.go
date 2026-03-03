5f9c19d7a47873bf050c7725a602c7f49948b9a2
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-03 19:46:07 +0000

    Add utility to remove duplicate lines from a text file

--- part6-cli1-Prototype/file_cleaner.go
+++ part6-cli1-Prototype/file_cleaner.go
@@ -122,4 +122,58 @@ func writeLines(filename string, lines []string) error {
 		}
 	}
 	return writer.Flush()
+}package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run file_cleaner.go <input_file>")
+		return
+	}
+
+	inputFile := os.Args[1]
+	outputFile := inputFile + ".cleaned"
+
+	inFile, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Printf("Error opening input file: %v\n", err)
+		return
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Printf("Error creating output file: %v\n", err)
+		return
+	}
+	defer outFile.Close()
+
+	seen := make(map[string]bool)
+	scanner := bufio.NewScanner(inFile)
+	writer := bufio.NewWriter(outFile)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !seen[line] {
+			seen[line] = true
+			_, err := writer.WriteString(line + "\n")
+			if err != nil {
+				fmt.Printf("Error writing to output file: %v\n", err)
+				return
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading input file: %v\n", err)
+		return
+	}
+
+	writer.Flush()
+	fmt.Printf("Duplicate lines removed. Cleaned file saved as: %s\n", outputFile)
 }

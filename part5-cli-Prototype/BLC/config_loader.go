eb06f0d16cadfdd25c89e4e733abef0916b14510
FelixMakes <felix.makes@hotmail.com>
2026-03-04 23:25:11 +0000

    Add configuration file parsing with YAML support

--- part5-cli-Prototype/BLC/config_loader.go
+++ part5-cli-Prototype/BLC/config_loader.go
@@ -570,4 +570,57 @@ func traverseAndOverride(obj interface{}, prefix string) {
 			}
 		}
 	}
+}package config
+
+import (
+    "fmt"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
+)
+
+type AppConfig struct {
+    Server struct {
+        Port    int    `yaml:"port"`
+        Host    string `yaml:"host"`
+        Timeout int    `yaml:"timeout"`
+    } `yaml:"server"`
+    Database struct {
+        Host     string `yaml:"host"`
+        Port     int    `yaml:"port"`
+        Username string `yaml:"username"`
+        Password string `yaml:"password"`
+        Name     string `yaml:"name"`
+    } `yaml:"database"`
+    Logging struct {
+        Level  string `yaml:"level"`
+        Output string `yaml:"output"`
+    } `yaml:"logging"`
+}
+
+func LoadConfig(filePath string) (*AppConfig, error) {
+    data, err := ioutil.ReadFile(filePath)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read config file: %w", err)
+    }
+
+    var config AppConfig
+    err = yaml.Unmarshal(data, &config)
+    if err != nil {
+        return nil, fmt.Errorf("failed to parse YAML: %w", err)
+    }
+
+    return &config, nil
+}
+
+func (c *AppConfig) Validate() error {
+    if c.Server.Port <= 0 || c.Server.Port > 65535 {
+        return fmt.Errorf("invalid server port: %d", c.Server.Port)
+    }
+    if c.Database.Host == "" {
+        return fmt.Errorf("database host cannot be empty")
+    }
+    if c.Database.Name == "" {
+        return fmt.Errorf("database name cannot be empty")
+    }
+    return nil
 }

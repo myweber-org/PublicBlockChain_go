6ea357b426f32593eba74d338e8056f6f7b80024
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-03-04 22:36:39 +0000

    Add data processing utility for cleaning and validating input strings

--- part4-DataPersistence-Prototype/BLC/data_processor.go
+++ part4-DataPersistence-Prototype/BLC/data_processor.go
@@ -467,3 +467,36 @@ func CalculateAverage(records []DataRecord) float64 {
     }
     return total / float64(len(records))
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+	emailRegex      *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+		emailRegex:      regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`),
+	}
+}
+
+func (dp *DataProcessor) CleanString(input string) string {
+	trimmed := strings.TrimSpace(input)
+	return dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+}
+
+func (dp *DataProcessor) ValidateEmail(email string) bool {
+	return dp.emailRegex.MatchString(email)
+}
+
+func (dp *DataProcessor) NormalizeEmail(email string) (string, bool) {
+	cleaned := dp.CleanString(email)
+	lowerEmail := strings.ToLower(cleaned)
+	return lowerEmail, dp.ValidateEmail(lowerEmail)
+}

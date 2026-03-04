f41924ee9f445377bad991fe064029a47023bdea
heyQuim <hey.quim@hotmail.com>
2026-03-04 22:19:10 +0000

    Add user activity logging middleware for tracking API usage

--- part12-MerkleTree_Prototype/user_activity_logger.go
+++ part12-MerkleTree_Prototype/user_activity_logger.go
@@ -81,3 +81,48 @@ func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("User Activity - Method: %s, Path: %s, IP: %s, User-Agent: %s, Duration: %v",
 		r.Method, r.URL.Path, ipAddress, userAgent, duration)
 }
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	recorder := &responseRecorder{
+		ResponseWriter: w,
+		statusCode:     http.StatusOK,
+	}
+
+	al.handler.ServeHTTP(recorder, r)
+
+	duration := time.Since(start)
+	log.Printf(
+		"Method: %s | Path: %s | Status: %d | Duration: %v | User-Agent: %s",
+		r.Method,
+		r.URL.Path,
+		recorder.statusCode,
+		duration,
+		r.UserAgent(),
+	)
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.statusCode = code
+	rr.ResponseWriter.WriteHeader(code)
+}

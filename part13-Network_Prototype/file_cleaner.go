d98feec6ef4b58d259495043e40b12eee7a9aafc
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-05 14:52:10 +0000

    Add utility to remove temporary files older than 7 days

--- part13-Network_Prototype/file_cleaner.go
+++ part13-Network_Prototype/file_cleaner.go
@@ -49,4 +49,52 @@ func main() {
 	}
 
 	fmt.Printf("Cleanup completed. Removed %d files, freed %d bytes.\n", removedCount, totalSize)
+}package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	tempDir      = "/tmp/app_cache"
+	maxAgeHours  = 168
+)
+
+func main() {
+	err := cleanOldFiles(tempDir, maxAgeHours)
+	if err != nil {
+		fmt.Printf("Cleanup failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles(dirPath string, maxAgeHours int) error {
+	cutoffTime := time.Now().Add(-time.Duration(maxAgeHours) * time.Hour)
+
+	return filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		info, err := d.Info()
+		if err != nil {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			fmt.Printf("Removing old file: %s (modified: %v)\n", path, info.ModTime())
+			return os.Remove(path)
+		}
+
+		return nil
+	})
 }

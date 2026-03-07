56106caca58ed05dddc4b5c78489edc622a2be49
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-03-07 20:59:19 +0000

    Add utility to remove temporary files older than 7 days

--- part1-Basic-Prototype/BLC/file_cleaner.go
+++ part1-Basic-Prototype/BLC/file_cleaner.go
@@ -2,82 +2,37 @@ package main
 
 import (
 	"fmt"
-	"io/fs"
 	"os"
 	"path/filepath"
 	"time"
 )
 
-const (
-	LogDir       = "./logs"
-	MaxAgeDays   = 30
-	ScanInterval = 24 * time.Hour
-)
+const retentionDays = 7
 
 func main() {
-	ticker := time.NewTicker(ScanInterval)
-	defer ticker.Stop()
+	tempDir := os.TempDir()
+	cutoffTime := time.Now().AddDate(0, 0, -retentionDays)
 
-	fmt.Println("Starting log cleanup service...")
-	cleanupOldLogs()
-
-	for range ticker.C {
-		cleanupOldLogs()
-	}
-}
-
-func cleanupOldLogs() {
-	cutoffTime := time.Now().AddDate(0, 0, -MaxAgeDays)
-
-	err := filepath.WalkDir(LogDir, func(path string, d fs.DirEntry, err error) error {
+	err := filepath.Walk(tempDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return nil
 		}
 
-		if d.IsDir() {
-			return nil
-		}
-
-		info, err := d.Info()
-		if err != nil {
+		if info.IsDir() {
 			return nil
 		}
 
 		if info.ModTime().Before(cutoffTime) {
-			if err := os.Remove(path); err != nil {
-				fmt.Printf("Failed to remove %s: %v\n", path, err)
-			} else {
-				fmt.Printf("Removed old log file: %s\n", path)
-			}
+			fmt.Printf("Removing old file: %s\n", path)
+			os.Remove(path)
 		}
+
 		return nil
 	})
 
 	if err != nil {
-		fmt.Printf("Error walking directory: %v\n", err)
+		fmt.Printf("Error cleaning temp files: %v\n", err)
+	} else {
+		fmt.Println("Temporary file cleanup completed")
 	}
-}package main
-
-import (
-    "os"
-    "path/filepath"
-    "time"
-)
-
-func main() {
-    dir := "/tmp"
-    cutoff := time.Now().AddDate(0, 0, -7)
-
-    filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return nil
-        }
-        if info.IsDir() {
-            return nil
-        }
-        if info.ModTime().Before(cutoff) {
-            os.Remove(path)
-        }
-        return nil
-    })
 }

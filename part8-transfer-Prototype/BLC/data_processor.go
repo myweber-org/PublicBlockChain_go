10f6c5d70bee35a3902afa8a0372fdcddf2a9f58
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-07 16:47:04 +0000

    Add data processing utility for cleaning and normalizing input strings

--- part8-transfer-Prototype/BLC/data_processor.go
+++ part8-transfer-Prototype/BLC/data_processor.go
@@ -337,3 +337,52 @@ func main() {
 
 	generateReport(records)
 }
+package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+type DataProcessor struct {
+	whitespaceRegex *regexp.Regexp
+}
+
+func NewDataProcessor() *DataProcessor {
+	return &DataProcessor{
+		whitespaceRegex: regexp.MustCompile(`\s+`),
+	}
+}
+
+func (dp *DataProcessor) CleanInput(input string) string {
+	trimmed := strings.TrimSpace(input)
+	normalized := dp.whitespaceRegex.ReplaceAllString(trimmed, " ")
+	return normalized
+}
+
+func (dp *DataProcessor) NormalizeCase(input string, toUpper bool) string {
+	cleaned := dp.CleanInput(input)
+	if toUpper {
+		return strings.ToUpper(cleaned)
+	}
+	return strings.ToLower(cleaned)
+}
+
+func (dp *DataProcessor) ExtractAlphanumeric(input string) string {
+	alnumRegex := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+	return alnumRegex.ReplaceAllString(input, "")
+}
+
+func main() {
+	processor := NewDataProcessor()
+	
+	sample := "  Hello   World! 123  "
+	cleaned := processor.CleanInput(sample)
+	normalized := processor.NormalizeCase(cleaned, false)
+	alnum := processor.ExtractAlphanumeric(sample)
+	
+	println("Original:", sample)
+	println("Cleaned:", cleaned)
+	println("Normalized:", normalized)
+	println("Alphanumeric only:", alnum)
+}

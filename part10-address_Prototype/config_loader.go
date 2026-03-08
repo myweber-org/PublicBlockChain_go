ac41f3fab8d0fb0c5fc9ffd015c7aa5ad46990e8
ElaraWrites <elara.writes@outlook.com>
2026-03-08 14:28:00 +0000

    Add configuration loader with YAML support and environment variable overrides

--- part10-address_Prototype/config_loader.go
+++ part10-address_Prototype/config_loader.go
@@ -120,4 +120,106 @@ func getEnvAsSlice(key string, defaultValue []string) []string {
         return defaultValue
     }
     return strings.Split(valueStr, ",")
+}package config
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Server struct {
+		Host string `yaml:"host" env:"SERVER_HOST"`
+		Port int    `yaml:"port" env:"SERVER_PORT"`
+	} `yaml:"server"`
+	Database struct {
+		Host     string `yaml:"host" env:"DB_HOST"`
+		Port     int    `yaml:"port" env:"DB_PORT"`
+		Name     string `yaml:"name" env:"DB_NAME"`
+		Username string `yaml:"username" env:"DB_USER"`
+		Password string `yaml:"password" env:"DB_PASS"`
+	} `yaml:"database"`
+	Logging struct {
+		Level  string `yaml:"level" env:"LOG_LEVEL"`
+		Output string `yaml:"output" env:"LOG_OUTPUT"`
+	} `yaml:"logging"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	if configPath == "" {
+		configPath = "config.yaml"
+	}
+
+	absPath, err := filepath.Abs(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := os.ReadFile(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, err
+	}
+
+	if err := overrideFromEnv(&cfg); err != nil {
+		return nil, err
+	}
+
+	if err := validateConfig(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
+
+func overrideFromEnv(cfg *Config) error {
+	envMap := map[string]interface{}{
+		"SERVER_HOST": &cfg.Server.Host,
+		"SERVER_PORT": &cfg.Server.Port,
+		"DB_HOST":     &cfg.Database.Host,
+		"DB_PORT":     &cfg.Database.Port,
+		"DB_NAME":     &cfg.Database.Name,
+		"DB_USER":     &cfg.Database.Username,
+		"DB_PASS":     &cfg.Database.Password,
+		"LOG_LEVEL":   &cfg.Logging.Level,
+		"LOG_OUTPUT":  &cfg.Logging.Output,
+	}
+
+	for envVar, target := range envMap {
+		if val := os.Getenv(envVar); val != "" {
+			switch t := target.(type) {
+			case *string:
+				*t = val
+			case *int:
+				var intVal int
+				if _, err := fmt.Sscanf(val, "%d", &intVal); err == nil {
+					*t = intVal
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func validateConfig(cfg *Config) error {
+	if cfg.Server.Host == "" {
+		return errors.New("server host cannot be empty")
+	}
+	if cfg.Server.Port <= 0 || cfg.Server.Port > 65535 {
+		return errors.New("server port must be between 1 and 65535")
+	}
+	if cfg.Database.Host == "" {
+		return errors.New("database host cannot be empty")
+	}
+	if cfg.Database.Name == "" {
+		return errors.New("database name cannot be empty")
+	}
+	return nil
 }

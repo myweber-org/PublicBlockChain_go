2e7c6745d48b6edf21f08beb4764bcf8ed13d9c3
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-03-12 19:02:35 +0000

    Add utility to remove temporary files older than 7 days

--- part10-address_Prototype/file_cleaner.go
+++ part10-address_Prototype/file_cleaner.go
@@ -38,4 +38,55 @@ func cleanOldFiles(dir string, maxAge time.Duration) error {
 
 		return nil
 	})
+}package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+const (
+	tempDir      = "/tmp/app_cache"
+	retentionDays = 7
+)
+
+func main() {
+	err := cleanOldFiles(tempDir)
+	if err != nil {
+		fmt.Printf("Cleanup failed: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Cleanup completed successfully")
+}
+
+func cleanOldFiles(dirPath string) error {
+	cutoffTime := time.Now().AddDate(0, 0, -retentionDays)
+
+	return filepath.WalkDir(dirPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		info, err := d.Info()
+		if err != nil {
+			return nil
+		}
+
+		if info.ModTime().Before(cutoffTime) {
+			err = os.Remove(path)
+			if err != nil {
+				fmt.Printf("Failed to remove %s: %v\n", path, err)
+			} else {
+				fmt.Printf("Removed: %s\n", path)
+			}
+		}
+		return nil
+	})
 }

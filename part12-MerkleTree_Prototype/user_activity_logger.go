fae895a229a04aaf7a614c617269dee0fd34ecd4
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-03-13 00:04:53 +0000

    Add user activity logging middleware with request details and timestamp

--- part12-MerkleTree_Prototype/user_activity_logger.go
+++ part12-MerkleTree_Prototype/user_activity_logger.go
@@ -51,4 +51,33 @@ func logActivity(activity ActivityLog) {
 		activity.IPAddress,
 		activity.Method,
 		activity.Path)
+}package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type ActivityLogger struct {
+	handler http.Handler
+}
+
+func NewActivityLogger(handler http.Handler) *ActivityLogger {
+	return &ActivityLogger{handler: handler}
+}
+
+func (al *ActivityLogger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	al.handler.ServeHTTP(w, r)
+	duration := time.Since(start)
+
+	log.Printf(
+		"Activity: %s %s | IP: %s | Duration: %v | User-Agent: %s",
+		r.Method,
+		r.URL.Path,
+		r.RemoteAddr,
+		duration,
+		r.UserAgent(),
+	)
 }
